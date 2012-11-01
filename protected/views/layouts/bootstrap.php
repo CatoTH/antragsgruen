54c5fd7f8a8b92e5eb0f3b836c4c81c52283b23e
Tobias Hößl <tobias@hoessl.eu>
2012-11-01 12:48:42 +0000

    Erzeugt sonst fehlerhaften HTML-Code

--- protected/views/layouts/bootstrap.php
+++ protected/views/layouts/bootstrap.php
@@ -9,11 +9,13 @@
 
 	<title><?php echo CHtml::encode($this->pageTitle); ?></title>
 
-    <!-- ### neu ### -->
-    <!--[if lt IE 9]><script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
-    <link href="//cloud.webtype.com/css/a47fbad3-f89b-4172-b851-d8bd6b49eb71.css" rel="stylesheet" type="text/css" />
-    <!--[if lt IE 8]><link rel="stylesheet" href="/css/antraege-ie7.css"><![endif]-->
-    <!-- ### /neu ### -->
+	<!-- ### neu ### -->
+	<!--[if lt IE 9]>
+	<script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
+	<link href="//cloud.webtype.com/css/a47fbad3-f89b-4172-b851-d8bd6b49eb71.css" rel="stylesheet" type="text/css"/>
+	<!--[if lt IE 8]>
+	<link rel="stylesheet" href="/css/antraege-ie7.css"><![endif]-->
+	<!-- ### /neu ### -->
 
 </head>
 
@@ -29,38 +31,38 @@
 				  "visible" => !Yii::app()->user->isGuest),
 		 */
 		$items = array(
-			array('label'=> 'Start',
-				  'url'  => array('/site/index')),
-			array('label'=> 'Hilfe',
-				  'url'  => array('/site/hilfe')),
-			array('label'=> 'Administration',
-				  'url'  => array('/admin/'),
-				'visible' => (Yii::app()->user->getState("role") == "admin"),
+			array('label' => 'Start',
+				  'url'   => array('/site/index')),
+			array('label' => 'Hilfe',
+				  'url'   => array('/site/hilfe')),
+			array('label'   => 'Administration',
+				  'url'     => array('/admin/'),
+				  'visible' => (Yii::app()->user->getState("role") == "admin"),
 			),
-			array('label'  => 'Login',
-				  'url'    => array('/site/login'),
-				  'visible'=> Yii::app()->user->isGuest),
-			array('label'  => 'Logout',
-				  'url'    => array('/site/logout'),
-				  'visible'=> !Yii::app()->user->isGuest)
+			array('label'   => 'Login',
+				  'url'     => array('/site/login'),
+				  'visible' => Yii::app()->user->isGuest),
+			array('label'   => 'Logout',
+				  'url'     => array('/site/logout'),
+				  'visible' => !Yii::app()->user->isGuest)
 		);
 
 		$this->widget('bootstrap.widgets.TbNavbar', array(
-		'fixed'   => false,
-		'brand'   => "",
-		'brandUrl'=> '/',
-		'collapse'=> true,
-		'items'   => array(
-			array(
-				'class'=> 'bootstrap.widgets.TbMenu',
+			'fixed'    => false,
+			'brand'    => "",
+			'brandUrl' => '/',
+			'collapse' => false,
+			'items'    => array(
+				array(
+					'class' => 'bootstrap.widgets.TbMenu',
 
-				'items'=> $items,
-			)
-		),
-	)); ?>
+					'items' => $items,
+				)
+			),
+		)); ?>
 	</div>
 
-    <a href="/" class="logo"><img src="/css/img/logo.png" alt="Antragsgrün"></a>
+	<a href="/" class="logo"><img src="/css/img/logo.png" alt="Antragsgrün"></a>
 
 	<!-- mainmenu -->
 	<?php if (isset($this->breadcrumbs)): ?>
@@ -68,7 +70,7 @@
 		$top_name = (isset($this->breadcrumbs_topname) && $this->breadcrumbs_topname !== null ? $this->breadcrumbs_topname : "Anträge");
 		$this->widget('bootstrap.widgets.TbBreadcrumbs', array(
 			'homeLink' => CHtml::link($top_name, "/"),
-			'links'=> $this->breadcrumbs,
+			'links'    => $this->breadcrumbs,
 		)); ?><!-- breadcrumbs -->
 	<?php endif?>
 
@@ -82,14 +84,14 @@
 	<div style="clear: both; padding-top: 15px;"></div>
 
 	<?php $this->widget('bootstrap.widgets.TbNavbar', array(
-	'htmlOptions'=> array(
-		'class'=> 'footer_bar',
+	'htmlOptions' => array(
+		'class' => 'footer_bar',
 	),
-	'fixed'   => false,
-	'brand'   => "",
-	'collapse'=> true,
-		'items'   => array('<a href="/site/impressum">Impressum</a>'),
-	)); ?>
+	'fixed'       => false,
+	'brand'       => "",
+	'collapse'    => false,
+	'items'       => array('<a href="/site/impressum">Impressum</a>'),
+)); ?>
 
 	<!-- footer -->
 
