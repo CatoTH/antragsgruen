916373eb95cb9ff7682b3471ba0ccdd888b675fd
Tobias Hößl <tobias@hoessl.eu>
2012-11-04 19:56:56 +0000

    Noch ein bisschen aufräumen

--- html/index.php
+++ html/index.php
@@ -1,5 +1,8 @@
 <?php
 
+defined('YII_DEBUG') or define('YII_DEBUG',true);
+defined('YII_TRACE_LEVEL') or define('YII_TRACE_LEVEL',3);
+
 setlocale(LC_ALL, 'de_DE');
 
 if (ini_get("max_execution_time") < 180) ini_set("max_execution_time", 180);
@@ -7,14 +10,8 @@ if (ini_get("max_execution_time") < 180) ini_set("max_execution_time", 180);
 ini_set('include_path', ini_get('include_path') . ":" . dirname(__FILE__) . "/../libraries/");
 require_once(dirname(__FILE__) . "/../vendor/autoload.php");
 
-// change the following paths if necessary
 $yii=dirname(__FILE__).'/../vendor/yiisoft/yii/framework/yii.php';
 $config=dirname(__FILE__).'/../protected/config/main.php';
-
-// remove the following lines when in production mode
-defined('YII_DEBUG') or define('YII_DEBUG',true);
-// specify how many levels of call stack should be shown in each log message
-defined('YII_TRACE_LEVEL') or define('YII_TRACE_LEVEL',3);
 
 require_once($yii);
 
