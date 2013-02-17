d99f000bb8fba3a9387cedf9e76c9a980c34af2b
Tobias Hößl <tobias@hoessl.eu>
2013-02-17 16:08:03 +0100

    Bugfix

--- protected/views/site/sidebar.php
+++ protected/views/site/sidebar.php
@@ -50,7 +50,7 @@ if ($veranstaltung->typ != Veranstaltung::$TYP_PROGRAMM) {
 $html = "<div class='well'><ul class='nav nav-list neue-aenderungsantraege'><li class='nav-header'>" . $sprache->get("Neue Änderungsanträge") . "</li>";
 if (count($neueste_aenderungsantraege) == 0) $html .= "<li><i>keine</i></li>";
 else foreach ($neueste_aenderungsantraege as $ant) {
-	$html .= "<li class='aeantrag'>" . CHtml::link("<strong>" . CHtml::encode($ant["revision_name"]) . "</strong> zu " . CHtml::encode($ant->antrag->revision_name), $this->createUrl("aenderungsantrag/anzeige", array("aenderungsantrag_id" => $antrag->id, "antrag_id" => $ant->antrag->id))) . "</li>\n";
+	$html .= "<li class='aeantrag'>" . CHtml::link("<strong>" . CHtml::encode($ant["revision_name"]) . "</strong> zu " . CHtml::encode($ant->antrag->revision_name), $this->createUrl("aenderungsantrag/anzeige", array("aenderungsantrag_id" => $ant->id, "antrag_id" => $ant->antrag->id))) . "</li>\n";
 }
 $html .= "</ul></div>";
 $this->menus_html[] = $html;
