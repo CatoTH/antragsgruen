082548caa1671bbc2ccdedbf1d35dd5c9275000b
Tobias Hößl <tobias@hoessl.eu>
2013-04-09 18:31:24 +0200

    Kommentare wurden nicht sortiert

--- protected/models/AntragKommentar.php
+++ protected/models/AntragKommentar.php
@@ -18,7 +18,11 @@ class AntragKommentar extends BaseAntragKommentar
 	 * @return array|AntragKommentar[]
 	 */
 	public static function holeNeueste($veranstaltung_id = 0, $limit = 0) {
-		$condition = ($limit > 0 ? array("limit" => $limit) : "");
+		$condition = array(
+			"order" => "datum DESC"
+		);
+		if ($limit > 0) $condition["limit"] = $limit;
+
 		$arr = AntragKommentar::model()->with(array(
 			"antrag" => array(
 				"condition" => "antrag.status NOT IN (" . implode(", ", IAntrag::$STATI_UNSICHTBAR) . ") AND antrag.veranstaltung = " . IntVal($veranstaltung_id)
