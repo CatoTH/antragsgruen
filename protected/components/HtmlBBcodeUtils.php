c8cbbac9e1d2a616b9625e7ef7cab021c9402c48
Tobias Hößl <tobias@hoessl.eu>
2014-05-19 21:37:13 +0200

    Doppelte Leerzeichen filtern

--- protected/components/HtmlBBcodeUtils.php
+++ protected/components/HtmlBBcodeUtils.php
@@ -30,9 +30,10 @@ class HtmlBBcodeUtils
 		$text = preg_replace("/\[(\/?)u\]/siu", "[\\1U]", $text);
 		$text = preg_replace("/\[(\/?)url([^\]]*)\]/siu", "[\\1URL\\2]", $text);
 		$text = preg_replace("/\[p\](.*)\[\/p\]/siuU", "\\1", $text);
-		
+
 		$text = str_replace(chr(194).chr(160), " ", $text);
-		
+		$text = preg_replace("/ {2,}/", " ", $text);
+
 		$x = explode("\n", $text);
 		foreach ($x as $i=>$zeile) {
 			$x[$i] = trim($zeile);
