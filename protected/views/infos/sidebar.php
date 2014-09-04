f5de05e23a1ef80fe7e4bd4678a0cb3a900ac344
Tobias Hößl <tobias@hoessl.eu>
2014-09-04 19:27:24 +0200

    Obsolet

--- protected/views/infos/sidebar.php
+++ protected/views/infos/sidebar.php
@@ -9,9 +9,5 @@ $html .= "<li class='nav-header'>Aktuelle Einsatzorte</li>";
 foreach ($reihen as $reihe) {
 	$html .= "<li>" . CHtml::link($reihe->name, $this->createUrl("veranstaltung/index", array("veranstaltungsreihe_id" => $reihe->subdomain))) . "</li>\n";
 }
-if (defined("ANTRAGSGRUEN_LEGACY_LINKS") && ANTRAGSGRUEN_LEGACY_LINKS) {
-	$html .= "<li><a href='http://www.konzepte-fuer-hessen.de'>LV Hessen: Programm Landtagswahlen '13</a></li>\n";
-	$html .= "<li><a href='http://www.transparenz-mv.de/'>Fraktion Mecklemburg-Vorpommern: Entwurf d. Transparenzgesetzes</a></li>\n";
-}
 $html .= '</ul>';
 $this->menus_html[] = $html;
