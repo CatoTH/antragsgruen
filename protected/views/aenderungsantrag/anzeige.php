14312c01667466c709900818899eabafdd11656f
Tobias Hoessl <tobias@hoessl.eu>
2015-02-19 21:51:03 +0100

    Versehentlich committetes Debug-Log entfernen

--- protected/views/aenderungsantrag/anzeige.php
+++ protected/views/aenderungsantrag/anzeige.php
@@ -152,9 +152,6 @@ if ($aenderungsantrag->antrag->veranstaltung->getEinstellungen()->ae_nummerierun
 		$zeit_von = time();
 		$absae = $aenderungsantrag->getAntragstextParagraphs_diff();
 		$diff = time() - $zeit_von;
-		$fp= fopen("/tmp/ae-render.log", "a");
-		fwrite($fp, $diff . ": " . $_SERVER["REQUEST_URI"] . "\n");
-		fclose($fp);
 
 		foreach ($absae as $i => $abs) if ($abs !== null) {
 			/** @var AenderungsantragAbsatz $abs */
