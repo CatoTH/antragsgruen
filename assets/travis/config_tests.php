a0983e7066afef61ace041680887050e4d34e68b
Tobias Hößl <tobias@hoessl.eu>
2015-06-27 09:04:46 +0200

    First attempt to support travis-ci

---
+++ assets/travis/config_tests.php
@@ -0,0 +1,19 @@
+<?php
+
+$params                        = new \app\models\settings\AntragsgruenApp();
+$params->domainPlain           = 'http://localhost/index-test.php/';
+$params->domainSubdomain       = 'http://localhost/index-test.php/';
+$params->prependWWWToSubdomain = false;
+$params->randomSeed            = '123456';
+$params->cookieValidationKey   = 'ljelkkjlj';
+$params->confirmEmailAddresses = true;
+$params->dbConnection          = [
+    'class'          => 'yii\db\Connection',
+    'dsn'            => 'mysql:host=localhost;dbname=antragsgruen_tests',
+    'emulatePrepare' => true,
+    'username'       => 'root',
+    'password'       => '',
+    'charset'        => 'utf8mb4',
+];
+
+return $params;
