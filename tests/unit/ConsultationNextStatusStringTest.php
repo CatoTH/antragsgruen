1f643436ba56e59454e7529bf0a6fac276c730e1
Tobias Hößl <tobias@hoessl.eu>
2015-07-24 21:35:42 +0200

    Fixing test case

---
+++ tests/unit/ConsultationNextStatusStringTest.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace unit;
+
+use Codeception\Util\Autoload;
+use app\models\db\Consultation;
+use app\models\db\Motion;
+use Yii;
+use Codeception\Specify;
+
+class ConsultationNextStatusStringTest extends DBTestBase
+{
+    use Specify;
+
+    /**
+     *
+     */
+    public function testMotionPrefix()
+    {
+        /** @var Consultation $consultation */
+        $consultation = Consultation::findOne(2);
+
+        // 'For first A',
+        $this->assertEquals('A1', $consultation->getNextMotionPrefix(3));
+
+
+        // 'For second A
+        $dummyMotion                 = new Motion();
+        $dummyMotion->title          = 'Testmotion';
+        $dummyMotion->status         = Motion::STATUS_SUBMITTED_UNSCREENED;
+        $dummyMotion->titlePrefix    = 'A1';
+        $dummyMotion->motionTypeId   = 3;
+        $dummyMotion->consultationId = $consultation->id;
+        $dummyMotion->cache          = '';
+        $dummyMotion->save();
+        $consultation->refresh();
+
+        $this->assertEquals('A2', $consultation->getNextMotionPrefix(3));
+
+
+        // 'For second B'
+
+        $dummyMotion                 = new Motion();
+        $dummyMotion->title          = 'Testmotion';
+        $dummyMotion->status         = Motion::STATUS_SUBMITTED_UNSCREENED;
+        $dummyMotion->titlePrefix    = 'B1';
+        $dummyMotion->motionTypeId   = 4;
+        $dummyMotion->consultationId = $consultation->id;
+        $dummyMotion->cache          = '';
+        $dummyMotion->save();
+        $consultation->refresh();
+
+        $this->assertEquals('B2', $consultation->getNextMotionPrefix(4));
+
+
+        // 'For third S'
+
+        $dummyMotion                 = new Motion();
+        $dummyMotion->title          = 'Testmotion';
+        $dummyMotion->status         = Motion::STATUS_SUBMITTED_UNSCREENED;
+        $dummyMotion->titlePrefix    = 'A4';
+        $dummyMotion->motionTypeId   = 3;
+        $dummyMotion->consultationId = $consultation->id;
+        $dummyMotion->cache          = '';
+        $dummyMotion->save();
+        $consultation->refresh();
+
+        $this->assertEquals('A5', $consultation->getNextMotionPrefix(3));
+
+
+        /*
+        $this->specify(
+            'For first R',
+            function () use ($consultation) {
+                $this->assertEquals('R1', $consultation->getNextAvailableStatusString(4));
+            }
+        );
+        */
+    }
+}
