8e25598b9e18fb88887d912dfc076f20e635ebf4
Tobias Hößl <tobias@hoessl.eu>
2015-07-25 15:37:26 +0200

    Finist test cases

--- tests/acceptance/admin/AmendmentEditInitiatorsCept.php
+++ tests/acceptance/admin/AmendmentEditInitiatorsCept.php
@@ -5,19 +5,56 @@ $I = new AcceptanceTester($scenario);
 $I->populateDBData1();
 
 $I->wantTo('edit an initiator');
-$page = $I->loginAndGotoStdAdminPage()->gotoMotionList()->gotoAmendmentEdit(2);
+$I->gotoConsultationHome();
+$I->loginAsStdAdmin();
+$I->gotoStdAdminPage()->gotoMotionTypes(1);
+$I->selectOption('#typeInitiatorForm', \app\models\initiatorForms\WithSupporters::getTitle());
+$I->submitForm('.adminTypeForm', [], 'save');
+
+$page = $I->gotoStdAdminPage()->gotoMotionList()->gotoAmendmentEdit(2);
 $I->see('E-Mail: testuser@example.org', '.supporterForm');
+$I->dontSeeElement('#initiatorOrga');
+$I->selectOption('#personTypeOrga', \app\models\db\ISupporter::PERSON_NATURAL);
 $I->fillField('#initiatorName', 'Another test user');
+$I->fillField('#initiatorOrga', 'KV Test');
 $I->fillField('#initiatorEmail', 'test2@example.org');
-$I->fillField('#resolutionDate', '23.05.1949');
+$I->fillField('#initiatorPhone', '01234567');
+
+$I->dontSeeElement('.initiatorData .initiatorRow');
+$I->executeJS('$(".initiatorData .adderRow a").click();');
+$I->seeElement('.initiatorData .initiatorRow');
+$I->fillField('.initiatorData .initiatorRow .name', 'My Friend');
+$I->fillField('.initiatorData .initiatorRow .organization', 'Her KV');
+
+
+$I->wantTo('add some suporters using full-text');
+
+$I->dontSeeElement('#fullTextHolder');
+$I->click('.fullTextAdder a');
+$I->seeElement('#fullTextHolder');
+
+$I->fillField('#fullTextHolder textarea', 'Tobias Hößl, KV München; Test 2');
+$I->click('#fullTextHolder .fullTextAdd');
+
+
 $I->submitForm('#amendmentUpdateForm', [], 'save');
+
+$I->wantTo('confirm the changes are saved');
+
+$page = $I->gotoStdAdminPage()->gotoMotionList()->gotoAmendmentEdit(2);
 $I->see('E-Mail: testuser@example.org', '.supporterForm');
 $I->seeInField('#initiatorName', 'Another test user');
+$I->seeInField('#initiatorOrga', 'KV Test');
 $I->seeInField('#initiatorEmail', 'test2@example.org');
-$I->seeInField('#resolutionDate', '23.05.1949');
+$I->seeInField('#initiatorPhone', '01234567');
 
-$scenario->incomplete('test cases not implemented yet');
-// General amendment data
-// More initiators
-// Supporters
-$I->see('E-Mail: testuser@example.org', '.supporterForm');
+$I->seeElement('.initiatorData .initiatorRow');
+$I->seeInField('.initiatorData .initiatorRow .name', 'My Friend');
+$I->seeInField('.initiatorData .initiatorRow .organization', 'Her KV');
+
+$name1 = $I->executeJS('return $(".supporterRow").eq(0).find("input.name").val()');
+$orga1 = $I->executeJS('return $(".supporterRow").eq(0).find("input.organization").val()');
+$name2 = $I->executeJS('return $(".supporterRow").eq(1).find("input.name").val()');
+if ($name1 != 'Tobias Hößl' || $orga1 != 'KV München' || $name2 != 'Test 2') {
+    $I->fail('Got invalid supporter Data: ' . $name1 . ' (' . $orga1 . ') / ' . $name2);
+}
--- tests/acceptance/admin/MotionEditInitiatorsCept.php
+++ tests/acceptance/admin/MotionEditInitiatorsCept.php
@@ -5,17 +5,50 @@ $I = new AcceptanceTester($scenario);
 $I->populateDBData1();
 
 $I->wantTo('edit an initiator');
-$page = $I->loginAndGotoStdAdminPage()->gotoMotionList()->gotoMotionEdit(58);
+$I->gotoConsultationHome(true, 'bdk', 'bdk');
+$I->loginAsStdAdmin();
+$page = $I->gotoStdAdminPage(true, 'bdk', 'bdk')->gotoMotionList()->gotoMotionEdit(4);
 $I->see('E-Mail: testuser@example.org', '.supporterForm');
 $I->fillField('#initiatorName', 'Another test user');
+$I->fillField('#initiatorOrga', 'KV Test');
 $I->fillField('#initiatorEmail', 'test2@example.org');
+$I->fillField('#initiatorPhone', '01234567');
+
+$I->dontSeeElement('.initiatorData .initiatorRow');
+$I->executeJS('$(".initiatorData .adderRow a").click();');
+$I->seeElement('.initiatorData .initiatorRow');
+$I->fillField('.initiatorData .initiatorRow .name', 'My Friend');
+$I->fillField('.initiatorData .initiatorRow .organization', 'Her KV');
+
+
+$I->wantTo('add some suporters using full-text');
+
+$I->dontSeeElement('#fullTextHolder');
+$I->click('.fullTextAdder a');
+$I->seeElement('#fullTextHolder');
+
+$I->fillField('#fullTextHolder textarea', 'Tobias Hößl, KV München; Test 2');
+$I->click('#fullTextHolder .fullTextAdd');
+
+
 $I->submitForm('#motionUpdateForm', [], 'save');
+
+$I->wantTo('confirm the changes are saved');
+
+$page = $I->gotoStdAdminPage(true, 'bdk', 'bdk')->gotoMotionList()->gotoMotionEdit(4);
 $I->see('E-Mail: testuser@example.org', '.supporterForm');
 $I->seeInField('#initiatorName', 'Another test user');
+$I->seeInField('#initiatorOrga', 'KV Test');
 $I->seeInField('#initiatorEmail', 'test2@example.org');
+$I->seeInField('#initiatorPhone', '01234567');
 
-$scenario->incomplete('test cases not implemented yet');
-// General motion data
-// More initiators
-// Supporters
-$I->see('E-Mail: testuser@example.org', '.supporterForm');
+$I->seeElement('.initiatorData .initiatorRow');
+$I->seeInField('.initiatorData .initiatorRow .name', 'My Friend');
+$I->seeInField('.initiatorData .initiatorRow .organization', 'Her KV');
+
+$name1 = $I->executeJS('return $(".supporterRow").eq(0).find("input.name").val()');
+$orga1 = $I->executeJS('return $(".supporterRow").eq(0).find("input.organization").val()');
+$name2 = $I->executeJS('return $(".supporterRow").eq(1).find("input.name").val()');
+if ($name1 != 'Tobias Hößl' || $orga1 != 'KV München' || $name2 != 'Test 2') {
+    $I->fail('Got invalid supporter Data: ' . $name1 . ' (' . $orga1 . ') / ' . $name2);
+}
