19e14b9c1a87be895e36a7772e4f8bd41afc8030
Tobias Hößl <tobias@hoessl.eu>
2015-08-07 18:40:18 +0200

    Improving merged inline diff

--- tests/unit/AmendmentDiffMergerTest.php
+++ tests/unit/AmendmentDiffMergerTest.php
@@ -4,7 +4,6 @@ namespace unit;
 
 use app\components\diff\AmendmentDiffMerger;
 use app\components\HTMLTools;
-use app\models\db\AmendmentSection;
 use Codeception\Specify;
 
 class AmendmentDiffMergerTest extends TestBase
@@ -79,6 +78,57 @@ class AmendmentDiffMergerTest extends TestBase
                 [
                     'amendment' => 0,
                     'text'      => 'ognudelt Ledahosn noch da Giasinga Heiwog</p>',
+                ]
+            ]
+        ], $groupedParaData);
+    }
+
+    /**
+     */
+    public function testMerge3()
+    {
+        $origText = '<p>Woibbadinga damischa owe gwihss Sauwedda ded Charivari dei heid gfoids ma sagrisch guad. Maßkruag wo hi mim Radl foahn Landla Leonhardifahrt, Radler. Ohrwaschl und glei wirds no fui lustiga Spotzerl Fünferl, so auf gehds beim Schichtl do legst di nieda ned Biawambn Breihaus. I mechad dee Schwoanshaxn ghupft wia gsprunga measi gschmeidig hawadere midananda vui huift vui Biawambn, des wiad a Mordsgaudi is. Biaschlegl soi oans, zwoa, gsuffa Oachkatzlschwoaf hod Wiesn.</p>
+<p>Oamoi großherzig Mamalad, liberalitas Bavariae hoggd!</p>';
+
+        $paragraphs = HTMLTools::sectionSimpleHTML($origText);
+
+        $affectedParagraphs = [
+            0 => "<p>New line at beginning</p><p>Woibbadinga damischa owe gwihss Sauwedda ded Charivari dei heid gfoids ma sagrisch guad. Maßkruag wo hi mim Radl foahn Landla Leonhardifahrt, Radler. Ohrwaschl und glei wirds no fui lustiga Spotzerl Fünferl, so auf gehds beim Schichtl do legst di nieda ned Biawambn Breihaus. I mechad dee Schwoanshaxn ghupft wia gsprunga measi gschmeidig hawadere midananda vui huift vui Biawambn, des wiad a Mordsgaudi is. Biaschlegl soi oans, zwoa, gsuffa Oachkatzlschwoaf hod Wiesn.</p><p>Neuer Absatz</p>",
+        ];
+
+        $merger = new AmendmentDiffMerger();
+        $merger->initByMotionParagraphs($paragraphs);
+        $merger->addAmendmentParagraphs(3, $affectedParagraphs);
+
+        $groupedParaData = $merger->getGroupedParagraphData();
+
+        $this->assertEquals([
+            [
+                [
+                    'amendment' => 0,
+                    'text'      => '',
+                ],
+                [
+                    'amendment' => 3,
+                    'text'      => '<p><ins>New line at beginning</ins></p>',
+                ],
+                [
+                    'amendment' => 0,
+                    'text'      => '<p>Woibbadinga damischa owe gwihss Sauwedda ded Charivari dei heid gfoids ma sagrisch guad. Maßkruag wo hi mim Radl foahn Landla Leonhardifahrt, Radler. Ohrwaschl und glei wirds no fui lustiga Spotzerl Fünferl, so auf gehds beim Schichtl do legst di nieda ned Biawambn Breihaus. I mechad dee Schwoanshaxn ghupft wia gsprunga measi gschmeidig hawadere midananda vui huift vui Biawambn, des wiad a Mordsgaudi is. Biaschlegl soi oans, zwoa, gsuffa Oachkatzlschwoaf hod Wiesn.</p>',
+                ],
+                [
+                    'amendment' => 3,
+                    'text'      => '<p><ins>Neuer Absatz</ins></p>',
+                ],
+                [
+                    'amendment' => 0,
+                    'text'      => '',
+                ]
+            ],
+            [
+                [
+                    'amendment' => 0,
+                    'text'      => '<p>Oamoi großherzig Mamalad, liberalitas Bavariae hoggd!</p>'
                 ]
             ]
         ], $groupedParaData);
