a30c8a351c24949ae2fdb2f4c230fa8e2ae119d9
Tobias Hößl <tobias@hoessl.eu>
2015-08-07 11:12:58 +0200

    Refactoring Merged Inline Diff

--- views/motion/showSimpleTextSectionInline.php
+++ views/motion/showSimpleTextSectionInline.php
@@ -7,10 +7,7 @@
 
 use app\components\UrlHelper;
 use app\models\db\Amendment;
-use app\models\db\ConsultationSettingsMotionSection;
-use app\models\db\User;
 use app\models\forms\CommentForm;
-use app\views\motion\LayoutHelper;
 use yii\helpers\Html;
 
 $paragraphs = $section->getTextParagraphObjects(false, true, true);
@@ -19,106 +16,17 @@ $classes    = ['paragraph'];
 /** @var Amendment[] $amendmentsById */
 $amendmentsById = [];
 
-$paras          = $section->getTextParagraphs();
-$paraData       = [];
-foreach ($paras as $paraNo => $paraStr) {
-    $origTokenized = \app\components\diff\Diff::tokenizeLine($paraStr);
-    $origArr       = preg_split('/\R/', $origTokenized);
-    $modifications = [];
-    foreach ($origArr as $x) {
-        $modifications[] = [
-            'orig'          => $x,
-            'modifications' => [],
-        ];
-    }
-    $paraData[$paraNo] = [
-        'orig'              => $paraStr,
-        'origTokenized'     => $origTokenized,
-        'modifications'     => $modifications,
-        'amendmentSections' => [],
-    ];
-}
+$merger = new \app\components\diff\AmendmentDiffMerger();
+$merger->initByMotionSection($section);
+
 
 $diffEngine = new \app\components\diff\Engine();
 foreach ($section->amendingSections as $sect) {
     $amendmentsById[$sect->amendmentId] = $sect->amendment;
-
-    $affectedParas = $sect->getAffectedParagraphs($paras);
-    foreach ($affectedParas as $amendPara => $amendText) {
-        $newTokens  = \app\components\diff\Diff::tokenizeLine($amendText);
-        $diffTokens = $diffEngine->compareStrings($paraData[$amendPara]['origTokenized'], $newTokens);
-        $origNo     = 0;
-        foreach ($diffTokens as $token) {
-            if ($token[1] == \app\components\diff\Engine::INSERTED) {
-                if ($token[0] != '') {
-                    $insStr = '<ins>' . $token[0] . '</ins>';
-                    if ($origNo == 0) {
-                        // @TODO
-                    } else {
-                        $pre = $origNo - 1;
-                        if (!isset($paraData[$amendPara]['modifications'][$pre]['modifications'][$sect->amendmentId])) {
-                            $orig                                                                             = $paraData[$amendPara]['modifications'][$pre]['orig'];
-                            $paraData[$amendPara]['modifications'][$pre]['modifications'][$sect->amendmentId] = $orig;
-                        }
-                        $paraData[$amendPara]['modifications'][$pre]['modifications'][$sect->amendmentId] .= $insStr;
-                    }
-                }
-            }
-            if ($token[1] == \app\components\diff\Engine::DELETED) {
-                if ($token[0] != '') {
-                    $delStr = '<del>' . $token[0] . '</del>';
-                    if (!isset($paraData[$amendPara]['modifications'][$origNo]['modifications'][$sect->amendmentId])) {
-                        $paraData[$amendPara]['modifications'][$origNo]['modifications'][$sect->amendmentId] = '';
-                    }
-                    $paraData[$amendPara]['modifications'][$origNo]['modifications'][$sect->amendmentId] .= $delStr;
-                }
-                $origNo++;
-            }
-            if ($token[1] == \app\components\diff\Engine::UNMODIFIED) {
-                $origNo++;
-            }
-        }
-    }
+    $merger->addAmendmentSection($sect);
 }
 
-$groupedParaData = [];
-foreach ($paraData as $paraNo => $para) {
-    $paraG            = [];
-    $pending          = '';
-    $pendingCurrAmend = 0;
-    foreach ($para['modifications'] as $modi) {
-        if (count($modi['modifications']) > 1) {
-            echo 'PROBLEM: ';
-            var_dump($modi);
-        } elseif (count($modi['modifications']) == 1) {
-            $keys = array_keys($modi['modifications']);
-            if ($keys[0] != $pendingCurrAmend) {
-                $paraG[]          = [
-                    'amendment' => $pendingCurrAmend,
-                    'text'      => $pending,
-                ];
-                $pending          = '';
-                $pendingCurrAmend = $keys[0];
-            }
-            $pending .= $modi['modifications'][$keys[0]];
-        } else {
-            if (0 != $pendingCurrAmend) {
-                $paraG[]          = [
-                    'amendment' => $pendingCurrAmend,
-                    'text'      => $pending,
-                ];
-                $pending          = '';
-                $pendingCurrAmend = 0;
-            }
-            $pending .= $modi['orig'];
-        }
-    }
-    $paraG[]                  = [
-        'amendment' => $pendingCurrAmend,
-        'text'      => $pending,
-    ];
-    $groupedParaData[$paraNo] = $paraG;
-}
+$groupedParaData = $merger->getGroupedParagraphData();
 
 foreach ($paragraphs as $paragraphNo => $paragraph) {
     $parClasses = $classes;
