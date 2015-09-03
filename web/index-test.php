28bb4ce7cac62295ac4eaf5314cdfd6dafbc9106
Tobias Hößl <tobias@hoessl.eu>
2015-09-03 15:18:37 +0500

    Output a more meaningfull message when an invalid configuration is encountered

--- web/index-test.php
+++ web/index-test.php
@@ -12,6 +12,14 @@ defined('YII_ENV') or define('YII_ENV', 'test');
 require(__DIR__ . '/../vendor/autoload.php');
 require(__DIR__ . '/../vendor/yiisoft/yii2/Yii.php');
 
-$config = require(__DIR__ . '/../tests/config/acceptance.php');
+try {
+    $config = require(__DIR__ . '/../tests/config/acceptance.php');
 
-(new yii\web\Application($config))->run();
+    (new yii\web\Application($config))->run();
+} catch (\yii\base\InvalidConfigException $e) {
+    echo 'Leider ist die Antragsgrün-Konfigurationsdatei (config/config.json) fehlerhaft.
+    Du kannst auf folgende Weisen versuchen, sie zu korrigieren:<ul>
+    <li>Die Datei von Hand bearbeiten und den Fehler ausfindig machen und korrigieren.</li>
+    <li>Den Installationsmodus aktivieren (die Datei config/INSTALLING anlegen) und eine beliebige Seite aufrufen, um in den Installationsmodus zu gelangen.</li>
+    </ul>';
+}
--- web/index.php
+++ web/index.php
@@ -14,6 +14,14 @@ if (!file_exists($configfile)) {
 require(__DIR__ . '/../vendor/autoload.php');
 require(__DIR__ . '/../vendor/yiisoft/yii2/Yii.php');
 
-$config = require(__DIR__ . '/../config/web.php');
+try {
+    $config = require(__DIR__ . '/../config/web.php');
 
-(new yii\web\Application($config))->run();
+    (new yii\web\Application($config))->run();
+} catch (\yii\base\InvalidConfigException $e) {
+    echo 'Leider ist die Antragsgrün-Konfigurationsdatei (config/config.json) fehlerhaft.
+    Du kannst auf folgende Weisen versuchen, sie zu korrigieren:<ul>
+    <li>Die Datei von Hand bearbeiten und den Fehler ausfindig machen und korrigieren.</li>
+    <li>Den Installationsmodus aktivieren (die Datei config/INSTALLING anlegen) und eine beliebige Seite aufrufen, um in den Installationsmodus zu gelangen.</li>
+    </ul>';
+}
