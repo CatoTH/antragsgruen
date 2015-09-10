f3f33e7880eb5280f3d062a20549a582080b679d
Tobias Hößl <tobias@hoessl.eu>
2015-09-10 15:36:47 +0800

    Bugfix: Motion sorting in agenda view

--- views/consultation/index_layout_agenda.php
+++ views/consultation/index_layout_agenda.php
@@ -53,7 +53,7 @@ if ($longVersion) {
         if (count($agendaItem->getVisibleMotions()) > 0) {
             echo '<h2 class="green">' . Html::encode($agendaItem->title) . '</h2>';
             echo '<ul class="motionListStd layout2">';
-            $motions = MotionSorter::getSortedMotions($consultation, $agendaItem->getVisibleMotions());
+            $motions = MotionSorter::getSortedMotionsFlat($consultation, $agendaItem->getVisibleMotions());
             foreach ($motions as $motion) {
                 LayoutHelper::showMotion($motion, $consultation);
                 $shownMotions[] = $motion->id;
