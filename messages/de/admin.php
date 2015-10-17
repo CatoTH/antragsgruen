3c61734e1b569c722493f1e011f08f8d0ee07e18
Tobias Hößl <tobias@hoessl.eu>
2015-10-17 18:21:54 +0700

    Replace strings

---
+++ messages/de/admin.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'saved'                   => 'Gespeichert.',
+    'amend_deleted'           => 'Der Änderungsantrag wurde gelöscht.',
+    'amend_screened'          => 'Der Änderungsantrag wurde freigeschaltet.',
+    'amend_prefix_collission' => 'Das angegebene Antragskürzel wird bereits von einem anderen Änderungsantrag verwendet.',
+];
