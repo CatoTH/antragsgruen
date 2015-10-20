1a4f80cacc305529bd9a57691a02a01ad3d5c9aa
joriki <joriki27@gmail.com>
2015-10-20 02:30:33 +0200

    Begriff der Programmkommission auch in Hinweisen zu Änderungsanträgen raus (und fehlenden Punkt ergänzt)

--- messages/de/amend.php
+++ messages/de/amend.php
@@ -10,15 +10,15 @@ return [
     'amendment_for'                     => 'Änderungsantrag zu',
     'confirmed_visible'                 => 'Du hast den Änderungsantrag eingereicht. Er ist jetzt sofort sichtbar.',
     'confirmed_screening'               => 'Du hast den Änderungsantrag eingereicht. ' .
-        'Er wird nun von der Programmkommission auf Zulässigkeit geprüft und dann freigeschaltet.',
+        'Er wird nun auf formale Richtigkeit geprüft und dann freigeschaltet.',
     'submitted_screening_email'         => "Hallo,\n\ndu hast soeben einen Änderungsantrag eingereicht.\n" .
-        "Die Programmkommission wird den Antrag auf Zulässigkeit prüfen und freischalten. " .
+        "Der Antrag wird nun auf formale Richtigkeit geprüft und dann freigeschaltet. " .
         "Du wirst dann gesondert darüber benachrichtigt.\n\n" .
         "Du kannst ihn hier einsehen: %LINK%\n\n" .
         "Mit freundlichen Grüßen,\n" .
         "  Das Antragsgrün-Team",
     'submitted_screening_email_subject' => 'Änderungsantrag eingereicht',
-    'screened_hint'                     => 'Von der Programmkommission geprüft',
+    'screened_hint'                     => 'Geprüft',
     'amend_for'                         => ' zu ',
     'create_explanation'                => 'Ändere hier den Antrag so ab, wie du ihn gern sehen würdest.<br>' . "\n" .
         'Unter &quot;<strong>Begründung</strong>&quot; kannst du die Änderung begründen.<br>' . "\n" .
