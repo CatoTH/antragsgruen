70afe283797829881674f78965ba3ea8d9158676
joriki <joriki27@gmail.com>
2015-10-24 20:04:58 +0200

    Organisationen fehlten als Antragstellerinnen in PDFs

--- models/db/Motion.php
+++ models/db/Motion.php
@@ -650,7 +650,7 @@ class Motion extends IMotion implements IRSSItem
         if (count($inits) == 1) {
             $first = $inits[0];
             if ($first->personType == MotionSupporter::PERSON_ORGANIZATION && $first->resolutionDate > 0) {
-                $return[\Yii::t('pdf', 'InitiatorSingle')] = $first->name;
+                $return[\Yii::t('pdf', 'InitiatorSingle')] = $first->organization;
                 $return[\Yii::t('pdf', 'ResolutionDate')]  = Tools::formatMysqlDate($first->resolutionDate);
             } else {
                 $return[\Yii::t('pdf', 'InitiatorSingle')] = $first->getNameWithResolutionDate(false);
