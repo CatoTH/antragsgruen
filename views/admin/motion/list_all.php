8c937750c569e385068cf0040e3c1cd345b0b72d
Tobias Hößl <tobias@hoessl.eu>
2015-10-25 11:42:01 +0100

    Replace strings

--- views/admin/motion/list_all.php
+++ views/admin/motion/list_all.php
@@ -17,9 +17,9 @@ use yii\helpers\Html;
 $controller = $this->context;
 $layout     = $controller->layoutParams;
 
-$this->title = 'Antragsliste';
-$layout->addBreadcrumb('Administration', UrlHelper::createUrl('admin/index'));
-$layout->addBreadcrumb('Antragsliste');
+$this->title = \Yii::t('admin', 'list_head_title');
+$layout->addBreadcrumb(\Yii::t('admin', 'bread_admin'), UrlHelper::createUrl('admin/index'));
+$layout->addBreadcrumb(\Yii::t('admin', 'bread_list'));
 $layout->loadTypeahead();
 $layout->loadFuelux();
 $layout->addJS('js/backend.js');
@@ -33,13 +33,14 @@ $layout->addOnLoadJS('$.AntragsgruenAdmin.motionListAll();');
 $route   = 'admin/motion/listall';
 $hasTags = (count($controller->consultation->tags) > 0);
 
-echo '<h1>' . 'Liste: Anträge, Änderungsanträge' . '</h1>';
+echo '<h1>' . \Yii::t('admin', 'list_head_title') . '</h1>';
 echo '<div class="content">';
 echo '<form method="GET" action="' . Html::encode(UrlHelper::createUrl($route)) . '" class="motionListSearchForm">';
 
 echo $search->getFilterFormFields();
 
-echo '<div style="float: left;"><br><button type="submit" class="btn btn-success">Suchen</button></div>';
+echo '<div style="float: left;"><br><button type="submit" class="btn btn-success">' .
+    \Yii::t('admin', 'list_search_do') . '</button></div>';
 
 echo '</form><br style="clear: both;">';
 
@@ -53,51 +54,51 @@ echo '<thead><tr>
     <th class="markCol"></th>
     <th class="typeCol">';
 if ($search->sort == AdminMotionFilterForm::SORT_TYPE) {
-    echo '<span style="text-decoration: underline;">Typ</span>';
+    echo '<span style="text-decoration: underline;">' . \Yii::t('admin', 'list_type') . '</span>';
 } else {
     $url = $search->getCurrentUrl($route, ['Search[sort]' => AdminMotionFilterForm::SORT_TYPE]);
-    echo Html::a('Typ', $url);
+    echo Html::a(\Yii::t('admin', 'list_type'), $url);
 }
 echo '</th><th class="prefixCol">';
 if ($search->sort == AdminMotionFilterForm::SORT_TITLE_PREFIX) {
-    echo '<span style="text-decoration: underline;">Antragsnr.</span>';
+    echo '<span style="text-decoration: underline;">' . \Yii::t('admin', 'list_prefix') . '</span>';
 } else {
     $url = $search->getCurrentUrl($route, ['Search[sort]' => AdminMotionFilterForm::SORT_TITLE_PREFIX]);
-    echo Html::a('Antragsnr.', $url);
+    echo Html::a(\Yii::t('admin', 'list_prefix'), $url);
 }
 echo '</th><th class="titleCol">';
 if ($search->sort == AdminMotionFilterForm::SORT_TITLE) {
-    echo '<span style="text-decoration: underline;">Titel</span>';
+    echo '<span style="text-decoration: underline;">' . \Yii::t('admin', 'list_title') . '</span>';
 } else {
     $url = $search->getCurrentUrl($route, ['Search[sort]' => AdminMotionFilterForm::SORT_TITLE]);
-    echo Html::a('Titel', $url);
+    echo Html::a(\Yii::t('admin', 'list_title'), $url);
 }
 echo '</th><th>';
 if ($search->sort == AdminMotionFilterForm::SORT_STATUS) {
-    echo '<span style="text-decoration: underline;">Status</span>';
+    echo '<span style="text-decoration: underline;">' . \Yii::t('admin', 'list_status') . '</span>';
 } else {
     $url = $search->getCurrentUrl($route, ['Search[sort]' => AdminMotionFilterForm::SORT_STATUS]);
-    echo Html::a('Status', $url);
+    echo Html::a(\Yii::t('admin', 'list_status'), $url);
 }
 echo '</th><th>';
 if ($search->sort == AdminMotionFilterForm::SORT_INITIATOR) {
-    echo '<span style="text-decoration: underline;">AntragstellerInnen</span>';
+    echo '<span style="text-decoration: underline;">' . \Yii::t('admin', 'list_initiators') . '</span>';
 } else {
     $url = $search->getCurrentUrl($route, ['Search[sort]' => AdminMotionFilterForm::SORT_INITIATOR]);
-    echo Html::a('AntragstellerInnen', $url);
+    echo Html::a(\Yii::t('admin', 'list_initiators'), $url);
 }
 if ($hasTags) {
     echo '</th><th>';
     if ($search->sort == AdminMotionFilterForm::SORT_TAG) {
-        echo '<span style="text-decoration: underline;">Thema</span>';
+        echo '<span style="text-decoration: underline;">' . \Yii::t('admin', 'list_tag') . '</span>';
     } else {
         $url = $search->getCurrentUrl($route, ['Search[sort]' => AdminMotionFilterForm::SORT_TAG]);
-        echo Html::a('Thema', $url);
+        echo Html::a(\Yii::t('admin', 'list_tag'), $url);
     }
 }
 echo '</th>
-    <th>Export</th>
-    <th class="actionCol">Aktion</th>
+    <th>' . \Yii::t('admin', 'list_export') . '</th>
+    <th class="actionCol">' . \Yii::t('admin', 'list_action') . '</th>
 </tr></thead>';
 
 $motionStati    = Motion::getStati();
@@ -110,7 +111,7 @@ foreach ($entries as $entry) {
         $editUrl = UrlHelper::createUrl(['admin/motion/update', 'motionId' => $entry->id]);
         echo '<tr class="motion' . $entry->id . '">';
         echo '<td><input type="checkbox" name="motions[]" value="' . $entry->id . '" class="selectbox"></td>';
-        echo '<td>A</td>';
+        echo '<td>' . \Yii::t('admin', 'list_motion_short') . '</td>';
         echo '<td class="prefixCol"><a href="' . Html::encode($viewUrl) . '">';
         echo Html::encode($entry->titlePrefix != '' ? $entry->titlePrefix : '-') . '</a></td>';
         echo '<td class="titleCol"><span>';
@@ -125,7 +126,7 @@ foreach ($entries as $entry) {
                 $initiators[] = $initiator->name;
             }
         }
-        echo '<td>' . Html::encode(implode(", ", $initiators)) . '</td>';
+        echo '<td>' . Html::encode(implode(', ', $initiators)) . '</td>';
         if ($hasTags) {
             $tags = [];
             foreach ($entry->tags as $tag) {
@@ -147,20 +148,21 @@ foreach ($entries as $entry) {
   <ul class="dropdown-menu">';
         if (in_array($entry->status, [Motion::STATUS_DRAFT, Motion::STATUS_SUBMITTED_UNSCREENED])) {
             $link = Html::encode($search->getCurrentUrl($route, ['motionScreen' => $entry->id]));
-            $name = Html::encode('Freischalten');
+            $name = Html::encode(\Yii::t('admin', 'list_screen'));
             echo '<li><a tabindex="-1" href="' . $link . '" class="screen">' . $name . '</a>';
         } else {
             $link = Html::encode($search->getCurrentUrl($route, ['motionUnscreen' => $entry->id]));
-            $name = Html::encode('Freischalten zurücknehmen');
+            $name = Html::encode(\Yii::t('admin', 'list_unscreen'));
             echo '<li><a tabindex="-1" href="' . $link . '" class="unscreen">' . $name . '</a>';
         }
         $link = Html::encode(UrlHelper::createUrl(['motion/create', 'adoptInitiators' => $entry->id]));
-        $name = Html::encode('Neuer Antrag auf dieser Basis');
+        $name = Html::encode(\Yii::t('admin', 'list_template_motion'));
         echo '<li><a tabindex="-1" href="' . $link . '" class="asTemplate">' . $name . '</a>';
 
         $delLink = Html::encode($search->getCurrentUrl($route, ['motionDelete' => $entry->id]));
         echo '<li><a tabindex="-1" href="' . $delLink . '" class="delete" ' .
-            'onClick="return confirm(\'Diesen Antrag wirklich löschen?\');">Löschen</a></li>';
+            'onClick="return confirm(\'' . addslashes(\Yii::t('admin', 'list_confirm_del_motion')) . '\');">' .
+            \Yii::t('admin', 'list_delete') . '</a></li>';
         echo '</ul></div></td>';
         echo '</tr>';
     }
@@ -170,7 +172,7 @@ foreach ($entries as $entry) {
         $viewUrl = UrlHelper::createAmendmentUrl($entry);
         echo '<tr class="amendment' . $entry->id . '">';
         echo '<td><input type="checkbox" name="amendments[]" value="' . $entry->id . '" class="selectbox"></td>';
-        echo '<td>ÄA</td>';
+        echo '<td>' . \Yii::t('admin', 'list_amend_short') . '</td>';
         echo '<td class="prefixCol"><a href="' . Html::encode($viewUrl) . '">';
         echo Html::encode($entry->titlePrefix != '' ? $entry->titlePrefix : '-') . '</a></td>';
         echo '<td class="titleCol">' .
@@ -199,22 +201,23 @@ foreach ($entries as $entry) {
   </button>
   <ul class="dropdown-menu">';
         if (in_array($entry->status, [Amendment::STATUS_DRAFT, Amendment::STATUS_SUBMITTED_UNSCREENED])) {
-            $name = Html::encode('Freischalten');
+            $name = Html::encode(\Yii::t('admin', 'list_screen'));
             $link = Html::encode($search->getCurrentUrl($route, ['amendmentScreen' => $entry->id]));
             echo '<li><a tabindex="-1" href="' . $link . '" class="screen">' . $name . '</a>';
         } else {
-            $name = Html::encode('Freischalten zurücknehmen');
+            $name = Html::encode(\Yii::t('admin', 'list_unscreen'));
             $link = Html::encode($search->getCurrentUrl($route, ['amendmentUnscreen' => $entry->id]));
             echo '<li><a tabindex="-1" href="' . $link . '" class="unscreen">' . $name . '</a>';
         }
-        $name   = Html::encode('Neuer Änderungsantrag auf dieser Basis');
+        $name   = Html::encode(\Yii::t('admin', 'list_template_amendment'));
         $params = ['amendment/create', 'motionId' => $entry->motionId, 'adoptInitiators' => $entry->id];
         $link   = Html::encode(UrlHelper::createUrl($params));
         echo '<li><a tabindex="-1" href="' . $link . '" class="asTemplate">' . $name . '</a>';
 
         $delLink = Html::encode($search->getCurrentUrl($route, ['amendmentDelete' => $entry->id]));
         echo '<li><a tabindex="-1" href="' . $delLink . '" ' .
-            'onClick="return confirm(\'Diesen Änderungsantrag wirklich löschen?\');">Löschen</a></li>';
+            'onClick="return confirm(\'' . addslashes(\Yii::t('admin', 'list_confirm_del_amend')) . '\');">' .
+            \Yii::t('admin', 'list_delete') . '</a></li>';
         echo '</ul></div></td>';
         echo '</tr>';
     }
@@ -226,14 +229,14 @@ echo '</table>';
 echo '<section style="overflow: auto;">';
 
 echo '<div style="float: left; line-height: 40px; vertical-align: middle;">';
-echo '<a href="#" class="markAll">Alle</a> &nbsp; ';
-echo '<a href="#" class="markNone">Keines</a> &nbsp; ';
+echo '<a href="#" class="markAll">' . \Yii::t('admin', 'list_all') . '</a> &nbsp; ';
+echo '<a href="#" class="markNone">' . \Yii::t('admin', 'list_none') . '</a> &nbsp; ';
 echo '</div>';
 
-echo '<div style="float: right;">Markierte: &nbsp; ';
-echo '<button type="submit" class="btn btn-danger" name="delete">Löschen</button> &nbsp; ';
-echo '<button type="submit" class="btn btn-info" name="unscreen">Ent-Freischalten</button> &nbsp; ';
-echo '<button type="submit" class="btn btn-success" name="screen">Freischalten</button>';
+echo '<div style="float: right;">' . \Yii::t('admin', 'list_marked') . ': &nbsp; ';
+echo '<button type="submit" class="btn btn-danger" name="delete">' . \Yii::t('admin', 'list_delete') . '</button> &nbsp; ';
+echo '<button type="submit" class="btn btn-info" name="unscreen">' . \Yii::t('admin', 'list_unscreen') . '</button> &nbsp; ';
+echo '<button type="submit" class="btn btn-success" name="screen">' . \Yii::t('admin', 'list_screen') . '</button>';
 echo '</div>';
 echo '</section>';
 
