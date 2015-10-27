cec0264710aa618759085f9998205b2b57dc9812
Tobias Hößl <tobias@hoessl.eu>
2015-10-27 10:42:26 +0100

    Add test stubs

---
+++ tests/acceptance/amendments/DeadlineCept.php
@@ -0,0 +1,9 @@
+<?php
+
+/** @var \Codeception\Scenario $scenario */
+$I = new AcceptanceTester($scenario);
+$I->populateDBData1();
+
+$scenario->incomplete('not implemented yet');
+
+$I->see('dummy');
