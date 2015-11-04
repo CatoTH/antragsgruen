0739be57074a57fe9e83237b9c6fd932f6ca1437
Tobias Hößl <tobias@hoessl.eu>
2015-11-04 10:23:32 +0100

    Add missing migration

---
+++ migrations/m151104_092212_motion_type_deletable.php
@@ -0,0 +1,18 @@
+<?php
+
+use yii\db\Schema;
+use yii\db\Migration;
+
+class m151104_092212_motion_type_deletable extends Migration
+{
+    public function safeUp()
+    {
+        $this->addColumn('consultationMotionType', 'status', 'smallint');
+        $this->update('consultationMotionType', ['status' => 0]);
+    }
+
+    public function safeDown()
+    {
+        $this->dropColumn('consultationMotionType', 'status');
+    }
+}
