a8c869621abf298e6667f3389e776c99925a2c9a
Tobias Hößl <tobias@hoessl.eu>
2016-02-20 11:15:44 +0100

    Unslash request vars

--- components/wordpress/Request.php
+++ components/wordpress/Request.php
@@ -5,6 +5,7 @@ namespace app\components\wordpress;
 use Yii;
 use yii\base\InvalidConfigException;
 use yii\web\Cookie;
+use yii\web\NotFoundHttpException;
 
 class Request extends \yii\web\Request
 {
@@ -48,5 +49,96 @@ class Request extends \yii\web\Request
         }
 
         return $cookies;
+    }
+
+    private $_bodyParams;
+
+    /**
+     * Returns the request parameters given in the request body.
+     *
+     * Request parameters are determined using the parsers configured in [[parsers]] property.
+     * If no parsers are configured for the current [[contentType]] it uses the PHP function `mb_parse_str()`
+     * to parse the [[rawBody|request body]].
+     * @return array the request parameters given in the request body.
+     * @throws \yii\base\InvalidConfigException if a registered parser does not implement the [[RequestParserInterface]].
+     * @see getMethod()
+     * @see getBodyParam()
+     * @see setBodyParams()
+     */
+    public function getBodyParams()
+    {
+        if ($this->_bodyParams === null) {
+            $bodyParams        = parent::getBodyParams();
+            $this->_bodyParams = \wp_unslash($bodyParams);
+        }
+
+        return $this->_bodyParams;
+    }
+
+    /**
+     * Sets the request body parameters.
+     *
+     * @param array $values the request body parameters (name-value pairs)
+     *
+     * @see getBodyParam()
+     * @see getBodyParams()
+     */
+    public function setBodyParams($values)
+    {
+        $this->_bodyParams = $values;
+    }
+
+
+    private $_queryParams;
+
+    /**
+     * Returns the request parameters given in the [[queryString]].
+     *
+     * This method will return the contents of `$_GET` if params where not explicitly set.
+     * @return array the request GET parameter values.
+     * @see setQueryParams()
+     */
+    public function getQueryParams()
+    {
+        if ($this->_queryParams === null) {
+            return \wp_unslash($_GET);
+        }
+
+        return $this->_queryParams;
+    }
+
+    /**
+     * Sets the request [[queryString]] parameters.
+     *
+     * @param array $values the request query parameters (name-value pairs)
+     *
+     * @see getQueryParam()
+     * @see getQueryParams()
+     */
+    public function setQueryParams($values)
+    {
+        $this->_queryParams = $values;
+    }
+
+    /**
+     * Resolves the current request into a route and the associated parameters.
+     * @return array the first element is the route, and the second is the associated parameters.
+     * @throws NotFoundHttpException if the request cannot be resolved.
+     */
+    public function resolve()
+    {
+        $result = Yii::$app->getUrlManager()->parseRequest($this);
+        if ($result !== false) {
+            list ($route, $params) = $result;
+            if ($this->_queryParams === null) {
+                $_GET = $params + $_GET; // preserve numeric keys
+            } else {
+                $this->_queryParams = $params + $this->_queryParams;
+            }
+
+            return [$route, $this->getQueryParams()];
+        } else {
+            throw new NotFoundHttpException(Yii::t('yii', 'Page not found.'));
+        }
     }
 }
