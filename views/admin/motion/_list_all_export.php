c0b7c62436a2482cb981a4b4d0352aec2147d84a
Tobias Hößl <tobias@hoessl.eu>
2016-04-01 21:27:30 +0200

    First draft

---
+++ views/admin/motion/_list_all_export.php
@@ -0,0 +1,136 @@
+<?php
+
+/** @var \app\controllers\Base $controller */
+use app\components\UrlHelper;
+use yii\helpers\Html;
+
+$controller   = $this->context;
+$consultation = $controller->consultation;
+
+?>
+<div class="motionListExportRow">
+    <span class="title pull-left">Export:</span>
+
+    <?php
+    $amendLink = UrlHelper::createUrl('admin/amendment/openslides');
+    $usersLink = UrlHelper::createUrl('admin/index/openslidesusers');
+    ?>
+    <div class="dropdown dropdown-menu-left exportOpenslidesDd pull-right">
+        <button class="btn btn-default dropdown-toggle" type="button" id="exportOpenslidesBtn"
+                data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
+            Openslides
+            <span class="caret"></span>
+        </button>
+        <ul class="dropdown-menu" aria-labelledby="exportOpenslidesBtn">
+            <li>
+                <?php
+                $add = '<br><small>' . \Yii::t('admin', 'index_export_oslides_usersh') . '</small>';
+                echo Html::a(\Yii::t('admin', 'index_export_oslides_users') . $add, $usersLink, ['class' => 'users']);
+                ?></li>
+            <?php
+            foreach ($consultation->motionTypes as $motionType) {
+                $motionTypeUrl = UrlHelper::createUrl(['admin/motion/openslides', 'motionTypeId' => $motionType->id]);
+                $title         = $motionType->titlePlural;
+                echo '<li>' .
+                    Html::a($title, $motionTypeUrl, ['class' => 'slidesMotionType' . $motionType->id]) .
+                    '</li>';
+            } ?>
+            <li>
+                <?= Html::a(\Yii::t('admin', 'index_export_oslides_amend'), $amendLink, ['class' => 'amendments']) ?>
+            </li>
+        </ul>
+    </div>
+
+    <div class="dropdown dropdown-menu-left exportAmendmentDd pull-right">
+        <button class="btn btn-default dropdown-toggle" type="button" id="exportAmendmentsBtn"
+                data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
+            Änderungsanträge
+            <span class="caret"></span>
+        </button>
+        <ul class="dropdown-menu" aria-labelledby="exportAmendmentsBtn">
+            <li><label>
+                    <input type="checkbox" class="withdrawn"> Zurückgezogene
+                </label></li>
+            <li role="separator" class="divider"></li>
+            <?php
+            $amendmentOdsLink  = UrlHelper::createUrl('admin/amendment/odslist');
+            $amendmentPDFLink  = UrlHelper::createUrl('admin/amendment/pdflist');
+            $pdfCollectionLink = UrlHelper::createUrl('amendment/pdfcollection');
+
+            if ($controller->getParams()->xelatexPath) {
+                $amendmentPDFZIPLink = UrlHelper::createUrl('admin/amendment/pdfziplist');
+                $ttle                = \Yii::t('admin', 'index_pdf_zip_list');
+                echo '<li>' .
+                    Html::a($title, $amendmentPDFZIPLink, ['class' => 'amendmentPdfZipList']) . '
+    </li>';
+            }
+            $amendmentODTZIPLink = UrlHelper::createUrl('admin/amendment/odtziplist');
+            $title               = \Yii::t('admin', 'index_odt_zip_list');
+            echo '<li>' .
+                Html::a($title, $amendmentODTZIPLink, ['class' => 'amendmentOdtZipList']) . '
+    </li>';
+            echo '<li>' .
+                Html::a(\Yii::t('admin', 'index_export_ods'), $amendmentOdsLink, ['class' => 'amendmentOds']) .
+                '</li>';
+
+            $title = \Yii::t('admin', 'index_pdf_list');
+            echo '<li>' . Html::a($title, $amendmentPDFLink, ['class' => 'amendmentPdfList']) . '</li>';
+            ?>
+        </ul>
+    </div>
+    <?php
+    for ($i = count($consultation->motionTypes) - 1; $i >= 0; $i--) {
+        $motionType = $consultation->motionTypes[$i];
+        ?>
+        <div class="dropdown dropdown-menu-left exportMotionDd pull-right">
+            <button class="btn btn-default dropdown-toggle" type="button" id="exportMotionBtn<?= $motionType->id ?>"
+                    data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
+                <?= Html::encode($motionType->titlePlural) ?>
+                <span class="caret"></span>
+            </button>
+            <ul class="dropdown-menu" aria-labelledby="exportMotionBtn<?= $motionType->id ?>">
+                <li><label>
+                        <input type="checkbox" class="withdrawn"> Zurückgezogene
+                    </label></li>
+                <li role="separator" class="divider"></li>
+                <?php
+                if ($controller->getParams()->xelatexPath) {
+                    $zipURL = UrlHelper::createUrl(['motion/pdfcollection', 'motionTypeId' => $motionType->id]);
+                    $title  = \Yii::t('admin', 'index_pdf_collection');
+                    echo '<li>';
+                    echo Html::a($title, $zipURL, ['class' => 'motionPDF' . $motionType->id]);
+                    echo '</li>';
+                }
+
+                if ($controller->getParams()->xelatexPath) {
+                    $zipURL = UrlHelper::createUrl(['admin/motion/pdfziplist', 'motionTypeId' => $motionType->id]);
+                    $title  = \Yii::t('admin', 'index_pdf_zip_list');
+                    echo '<li>';
+                    echo Html::a($title, $zipURL, ['class' => 'motionZIP' . $motionType->id]);
+                    echo '</li>';
+                }
+
+                $zipURL = UrlHelper::createUrl(['admin/motion/odtziplist', 'motionTypeId' => $motionType->id]);
+                $title  = \Yii::t('admin', 'index_odt_zip_list');
+                echo '<li>';
+                echo Html::a($title, $zipURL, ['class' => 'motionOdtZIP' . $motionType->id]);
+                echo '</li>';
+
+                $odsUrl = UrlHelper::createUrl(['admin/motion/odslist', 'motionTypeId' => $motionType->id]);
+                $title  = \Yii::t('admin', 'index_export_ods');
+                echo '<li>';
+                echo Html::a($title, $odsUrl, ['class' => 'motionODS' . $motionType->id]) . '</li>';
+
+                $excelUrl = UrlHelper::createUrl(['admin/motion/excellist', 'motionTypeId' => $motionType->id]);
+                $title    = \Yii::t('admin', 'index_export_excel');
+                echo '<li>';
+                echo Html::a($title, $excelUrl, ['class' => 'motionExcel' . $motionType->id]) .
+                    ' (' . \Yii::t('admin', 'index_error_prone') . ')</li>';
+                ?>
+            </ul>
+        </div>
+    <?php
+    }
+    ?>
+</div>
+
--- views/admin/motion/list_all.php
+++ views/admin/motion/list_all.php
@@ -34,6 +34,9 @@ $route   = 'admin/motion/listall';
 $hasTags = (count($controller->consultation->tags) > 0);
 
 echo '<h1>' . \Yii::t('admin', 'list_head_title') . '</h1>';
+
+echo $this->render('_list_all_export');
+
 echo '<div class="content">';
 echo '<form method="GET" action="' . Html::encode(UrlHelper::createUrl($route)) . '" class="motionListSearchForm">';
 
@@ -103,131 +106,18 @@ $lastMotion = null;
 
 foreach ($entries as $entry) {
     if (is_a($entry, Motion::class)) {
-        /** @var Motion $entry */
         $lastMotion = $entry;
-        $viewUrl    = UrlHelper::createMotionUrl($entry);
-        $editUrl    = UrlHelper::createUrl(['admin/motion/update', 'motionId' => $entry->id]);
-        echo '<tr class="motion motion' . $entry->id . '">';
-        echo '<td><input type="checkbox" name="motions[]" value="' . $entry->id . '" class="selectbox"></td>';
-        echo '<td>' . \Yii::t('admin', 'list_motion_short') . '</td>';
-        echo '<td class="prefixCol"><a href="' . Html::encode($viewUrl) . '">';
-        echo Html::encode($entry->titlePrefix != '' ? $entry->titlePrefix : '-') . '</a></td>';
-        echo '<td class="titleCol"><span>';
-        echo Html::a((trim($entry->title) != '' ? $entry->title : '-'), $editUrl);
-        echo '</span></td>';
-        echo '<td>' . Html::encode($motionStati[$entry->status]) . '</td>';
-        $initiators = [];
-        foreach ($entry->getInitiators() as $initiator) {
-            if ($initiator->personType == \app\models\db\ISupporter::PERSON_ORGANIZATION) {
-                $initiators[] = $initiator->organization;
-            } else {
-                $initiators[] = $initiator->name;
-            }
-        }
-        echo '<td>' . Html::encode(implode(', ', $initiators)) . '</td>';
-        if ($hasTags) {
-            $tags = [];
-            foreach ($entry->tags as $tag) {
-                $tags[] = $tag->title;
-            }
-            echo '<td>' . Html::encode(implode(', ', $tags)) . '</td>';
-        }
-        echo '<td class="exportCol">';
-        echo Html::a('PDF', UrlHelper::createMotionUrl($entry, 'pdf'), ['class' => 'pdf']) . ' / ';
-        echo Html::a('ODT', UrlHelper::createMotionUrl($entry, 'odt'), ['class' => 'odt']) . ' / ';
-        echo Html::a('HTML', UrlHelper::createMotionUrl($entry, 'plainhtml'), ['class' => 'plainHtml']);
-        echo '</td>';
-
-        echo '<td class="actionCol"><div class="btn-group">
-  <button class="btn btn-default dropdown-toggle" data-toggle="dropdown" aria-expanded="false">
-    Aktion
-    <span class="caret"></span>
-  </button>
-  <ul class="dropdown-menu">';
-        if (in_array($entry->status, [Motion::STATUS_DRAFT, Motion::STATUS_SUBMITTED_UNSCREENED])) {
-            $link = Html::encode($search->getCurrentUrl($route, ['motionScreen' => $entry->id]));
-            $name = Html::encode(\Yii::t('admin', 'list_screen'));
-            echo '<li><a tabindex="-1" href="' . $link . '" class="screen">' . $name . '</a>';
-        } else {
-            $link = Html::encode($search->getCurrentUrl($route, ['motionUnscreen' => $entry->id]));
-            $name = Html::encode(\Yii::t('admin', 'list_unscreen'));
-            echo '<li><a tabindex="-1" href="' . $link . '" class="unscreen">' . $name . '</a>';
-        }
-        $link = Html::encode(UrlHelper::createUrl(['motion/create', 'adoptInitiators' => $entry->id]));
-        $name = Html::encode(\Yii::t('admin', 'list_template_motion'));
-        echo '<li><a tabindex="-1" href="' . $link . '" class="asTemplate">' . $name . '</a>';
-
-        $delLink = Html::encode($search->getCurrentUrl($route, ['motionDelete' => $entry->id]));
-        echo '<li><a tabindex="-1" href="' . $delLink . '" class="delete" ' .
-            'onClick="return confirm(\'' . addslashes(\Yii::t('admin', 'list_confirm_del_motion')) . '\');">' .
-            \Yii::t('admin', 'list_delete') . '</a></li>';
-        echo '</ul></div></td>';
-        echo '</tr>';
+        echo $this->render('_list_all_item_motion', [
+            'entry'  => $entry,
+            'search' => $search,
+        ]);
     }
     if (is_a($entry, Amendment::class)) {
-        /** @var Amendment $entry */
-        $editUrl = UrlHelper::createUrl(['admin/amendment/update', 'amendmentId' => $entry->id]);
-        $viewUrl = UrlHelper::createAmendmentUrl($entry);
-        echo '<tr class="amendment amendment' . $entry->id . '">';
-        echo '<td><input type="checkbox" name="amendments[]" value="' . $entry->id . '" class="selectbox"></td>';
-        echo '<td>' . \Yii::t('admin', 'list_amend_short') . '</td>';
-        echo '<td class="prefixCol"><a href="' . Html::encode($viewUrl) . '">';
-        if ($lastMotion && $entry->motionId == $lastMotion->id) {
-            echo "&#8627;";
-        }
-        echo Html::encode($entry->titlePrefix != '' ? $entry->titlePrefix : '-') . '</a></td>';
-        echo '<td class="titleCol"><span>';
-        if ($lastMotion && $entry->motionId == $lastMotion->id) {
-            echo "&#8627;";
-        }
-        echo Html::a((trim($entry->getMyMotion()->title) != '' ? $entry->getMyMotion()->title : '-'), $editUrl) . '</span></td>';
-        echo '<td>' . Html::encode($amendmentStati[$entry->status]) . '</td>';
-        $initiators = [];
-        foreach ($entry->getInitiators() as $initiator) {
-            if ($initiator->personType == \app\models\db\ISupporter::PERSON_ORGANIZATION) {
-                $initiators[] = $initiator->organization;
-            } else {
-                $initiators[] = $initiator->name;
-            }
-        }
-        echo '<td>' . Html::encode(implode(', ', $initiators)) . '</td>';
-        if ($hasTags) {
-            echo '<td></td>';
-        }
-        echo '<td class="exportCol">';
-        echo Html::a('PDF', UrlHelper::createAmendmentUrl($entry, 'pdf'), ['class' => 'pdf']) . ' / ';
-        echo Html::a('ODT', UrlHelper::createAmendmentUrl($entry, 'odt'), ['class' => 'odt']);
-        echo '</td>';
-
-        echo '<td class="actionCol"><div class="btn-group">
-  <button class="btn btn-default dropdown-toggle" data-toggle="dropdown" aria-expanded="false">
-    Aktion
-    <span class="caret"></span>
-  </button>
-  <ul class="dropdown-menu">';
-        if (in_array($entry->status, [Amendment::STATUS_DRAFT, Amendment::STATUS_SUBMITTED_UNSCREENED])) {
-            $name = Html::encode(\Yii::t('admin', 'list_screen'));
-            $link = Html::encode($search->getCurrentUrl($route, ['amendmentScreen' => $entry->id]));
-            echo '<li><a tabindex="-1" href="' . $link . '" class="screen">' . $name . '</a>';
-        } else {
-            $name = Html::encode(\Yii::t('admin', 'list_unscreen'));
-            $link = Html::encode($search->getCurrentUrl($route, ['amendmentUnscreen' => $entry->id]));
-            echo '<li><a tabindex="-1" href="' . $link . '" class="unscreen">' . $name . '</a>';
-        }
-        $name = Html::encode(\Yii::t('admin', 'list_template_amendment'));
-        $link = Html::encode(UrlHelper::createUrl([
-            'amendment/create',
-            'motionSlug'      => $entry->getMyMotion()->getMotionSlug(),
-            'adoptInitiators' => $entry->id
-        ]));
-        echo '<li><a tabindex="-1" href="' . $link . '" class="asTemplate">' . $name . '</a>';
-
-        $delLink = Html::encode($search->getCurrentUrl($route, ['amendmentDelete' => $entry->id]));
-        echo '<li><a tabindex="-1" href="' . $delLink . '" ' .
-            'onClick="return confirm(\'' . addslashes(\Yii::t('admin', 'list_confirm_del_amend')) . '\');">' .
-            \Yii::t('admin', 'list_delete') . '</a></li>';
-        echo '</ul></div></td>';
-        echo '</tr>';
+        echo $this->render('_list_all_item_amendment', [
+            'entry'      => $entry,
+            'search'     => $search,
+            'lastMotion' => $lastMotion,
+        ]);
     }
 }
 
