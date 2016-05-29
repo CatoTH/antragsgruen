f2b453c1f4670a4364e3b61174baa538fa08c16f
Tobias Hößl <tobias@hoessl.eu>
2016-05-29 18:45:23 +0200

    Add a test case for forcing motions

--- models/settings/Consultation.php
+++ models/settings/Consultation.php
@@ -28,8 +28,7 @@ class Consultation
     public $screeningComments      = false;
     public $initiatorConfirmEmails = false;
     public $adminsMayEdit          = true;
-
-    public $forceMotion = null;
+    public $forceMotion            = null;
 
     // SETTINGS WITHOUT TEST CASES
 
