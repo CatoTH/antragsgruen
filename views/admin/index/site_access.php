caf551283760bb5515bf9ee57a5e1697f8e585b2
Tobias Hößl <tobias@hoessl.eu>
2016-06-27 12:10:40 +0200

    Bugfix: Wurzelwerk in SAML-environments

--- views/admin/index/site_access.php
+++ views/admin/index/site_access.php
@@ -71,7 +71,7 @@ if (User::getCurrentUser()->getAuthType() == SiteSettings::LOGIN_STD) {
 echo ' ' . \Yii::t('admin', 'siteacc_useraccounts') . '</label>
 </div>';
 
-if ($controller->getParams()->hasWurzelwerk) {
+if ($controller->getParams()->hasWurzelwerk || $controller->getParams()->hasSaml) {
     $method = SiteSettings::LOGIN_WURZELWERK;
     echo '<div class="checkbox wurzelwerk">
   <label>' . Html::checkbox('login[]', in_array($method, $settings->loginMethods), ['value' => $method]) .
