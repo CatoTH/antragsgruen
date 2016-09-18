60be3a28193506faf6ec20ea980d44608bc4c97c
Tobias Hößl <tobias@hoessl.eu>
2016-09-18 15:59:29 +0200

    Upgrade Fuelux, Datetimepicker, ColResizable

--- views/admin/motion/list_all.php
+++ views/admin/motion/list_all.php
@@ -22,7 +22,7 @@ $layout->addBreadcrumb(\Yii::t('admin', 'bread_list'));
 $layout->loadTypeahead();
 $layout->loadFuelux();
 $layout->addJS('js/backend.js');
-$layout->addJS('js/colResizable-1.5.min.js');
+$layout->addJS('js/colResizable-1.6.min.js');
 $layout->addCSS('css/backend.css');
 $layout->fullWidth  = true;
 $layout->fullScreen = true;
