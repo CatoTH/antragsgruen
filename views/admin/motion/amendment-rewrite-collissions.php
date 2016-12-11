1de670f033e3099a4d2fc343fe66271798ccbb33
Tobias Hößl <tobias@hoessl.eu>
2016-12-11 18:36:52 +0100

    Styling

--- views/admin/motion/amendment-rewrite-collissions.php
+++ views/admin/motion/amendment-rewrite-collissions.php
@@ -6,13 +6,13 @@
  * @var array[] $collissions
  */
 
+use app\components\Tools;
 use yii\helpers\Html;
 
 if (count($collissions) == 0) {
     echo '<div class="alert alert-success">' . 'Keine Konflikte zu bestehenden Änderungsanträgen' . '</div>';
     return;
 }
-$fixedWidth = true; // @TODO
 
 ?>
 <div class="alert alert-danger">
@@ -25,19 +25,30 @@ $fixedWidth = true; // @TODO
 </div>
 
 <?php
+
+$fixedWidthSections = [];
+foreach ($amendments[array_keys($amendments)[0]]->getActiveSections() as $section) {
+    if ($section->getSettings()->fixedWidth) {
+        $fixedWidthSections[] = $section->sectionId;
+    }
+}
+
 foreach ($collissions as $amendmentId => $sections) {
     $amendment = $amendments[$amendmentId];
-    echo '<h2>' . Html::encode($amendment->getTitle()) . '</h2>';
+    echo '<h2 class="green">' . Html::encode($amendment->getTitle()) . '</h2>';
+    echo '<div class="amendmentBy">' .
+        '(Eingereicht von' . ': ' . $amendment->getInitiatorsStr() . ', ' .
+        'am' . ': ' . Tools::formatMysqlDate($amendment->getDate()) . ')</div>';
     foreach ($sections as $sectionId => $paragraphs) {
         foreach ($paragraphs as $paragraphNo => $text) {
             echo '<section class="amendment-override-block">';
             echo '<textarea name="amendmentOverride[' . $amendmentId . '][' . $sectionId . '][' . $paragraphNo . ']" ';
             echo 'value="" class=""></textarea>';
             echo '<div id="amendmentOverride_' . $amendmentId . '_' . $sectionId . '_' . $paragraphNo . '" class="';
-            if ($fixedWidth) {
+            if (in_array($sectionId, $fixedWidthSections)) {
                 echo 'fixedWidthFont ';
             }
-            echo 'texteditor" title="' . 'Änderungsantrag anpassen' . '">';
+            echo 'texteditor texteditorBox" title="' . 'Änderungsantrag anpassen' . '">';
             echo $text;
             echo '</div></section>';
         }
