bc04d201d8dcf5db3118170dfe1e6d587ef56777
Tobias Hößl <tobias@hoessl.eu>
2017-02-05 19:24:28 +0100

    Update build script

--- docs/create-dist.sh
+++ docs/create-dist.sh
@@ -44,9 +44,6 @@ mkdir runtime
 chmod 775 runtime
 chmod 775 web/assets
 
-cd web/js/bower/intl/locale-data
-find . -type f ! -name "de*" -exec rm {} \;
-cd ../../../../../
 rm -R web/js/bower/moment/src/
 rm -R web/js/src
 rm -R vendor/phpoffice/phpexcel/unitTests/
