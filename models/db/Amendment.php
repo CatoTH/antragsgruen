7ba4664e2b61cd42858388ff05ee1a4262e03908
Tobias Hößl <tobias@hoessl.eu>
2017-04-05 21:12:57 +0200

    Bugfix: visibility after withdrawing amendments

--- models/db/Amendment.php
+++ models/db/Amendment.php
@@ -641,9 +641,9 @@ class Amendment extends IMotion implements IRSSItem
     public function withdraw()
     {
         if ($this->status == Amendment::STATUS_DRAFT) {
-            $this->status = static::STATUS_WITHDRAWN_INVISIBLE;
-        } elseif (in_array($this->status, $this->getMyConsultation()->getInvisibleMotionStati())) {
             $this->status = static::STATUS_DELETED;
+        } elseif (in_array($this->status, $this->getMyConsultation()->getInvisibleMotionStati())) {
+            $this->status = static::STATUS_WITHDRAWN_INVISIBLE;
         } else {
             $this->status = static::STATUS_WITHDRAWN;
         }
