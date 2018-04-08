f9921cec14b51f564719cffb2797b5095f2676bf
Tobias Hößl <tobias@hoessl.eu>
2018-04-08 17:11:34 -0500

    Downloading files

--- components/updater/UpdateInformation.php
+++ components/updater/UpdateInformation.php
@@ -34,4 +34,55 @@ class UpdateInformation
             throw new Internal("Only patch releases are supported");
         }
     }
+
+    /**
+     * @return string
+     */
+    private function getAbsolutePath()
+    {
+        $dir = __DIR__ . '/../../runtime/updates/';
+        if (!file_exists($dir)) {
+            mkdir($dir, 0755);
+        }
+        $base = explode("/", $this->url);
+        return $dir . $base[count($base) - 1];
+
+    }
+
+    /**
+     * return bool
+     */
+    public function isDownloaded()
+    {
+        if (!file_exists($this->getAbsolutePath())) {
+            return false;
+        }
+        $content = file_get_contents($this->getAbsolutePath());
+        return (base64_encode(sodium_crypto_generichash($content)) === $this->signature);
+    }
+
+    /**
+     * @throws \Exception
+     */
+    public function download()
+    {
+        $curlc = curl_init($this->url);
+        curl_setopt($curlc, CURLOPT_RETURNTRANSFER, true);
+        $resp = curl_exec($curlc);
+        $info = curl_getinfo($curlc);
+        curl_close($curlc);
+
+        if (!isset($info['http_code'])) {
+            throw new \Exception("The update could not be loaded");
+        }
+        if ($info['http_code'] !== 200) {
+            throw new \Exception("The update could not be loaded");
+        }
+
+        if (base64_encode(sodium_crypto_generichash($resp)) !== $this->signature) {
+            throw new \Exception("The update file has the wrong checksum");
+        }
+
+        file_put_contents($this->getAbsolutePath(), $resp);
+    }
 }
--- components/updater/available-updates.php
+++ components/updater/available-updates.php
@@ -1,7 +1,14 @@
 <?php
 
+/**
+ * @var string[] $errors
+ */
 $title = "Available Updates";
 require(__DIR__ . '/layout-header.php');
+
+foreach ($errors as $error) {
+    echo '<div class="alert alert-danger">' . htmlentities($error, ENT_COMPAT, 'UTF-8') . '</div>';
+}
 
 $updates = \app\components\updater\UpdateChecker::getAvailableUpdates();
 if (count($updates) === 0) {
@@ -13,7 +20,7 @@ if (count($updates) === 0) {
             Abort Update
         </button>
     </form>
-    
+
     <?php
     require(__DIR__ . '/layout-footer.php');
     die();
@@ -35,13 +42,34 @@ if (count($updates) === 0) {
             <tr>
                 <th><?= htmlentities($update->version, ENT_COMPAT, 'UTF-8') ?></th>
                 <td><?= nl2br(htmlentities($update->changelog, ENT_COMPAT, 'UTF-8')) ?></td>
-                <td>
-                    <form method="POST">
-                        <button type="submit" class="btn btn-primary">
-                            Perform update
-                        </button>
-                    </form>
-                </td>
+                <?php
+                if (!$update->isDownloaded()) {
+                    ?>
+                    <td>
+                        <form method="POST">
+                            <input type="hidden" name="version"
+                                   value="<?= htmlentities($update->version, ENT_COMPAT, 'UTF-8') ?>">
+                            <button type="submit" class="btn btn-primary" name="download_update">
+                                Download
+                            </button>
+                        </form>
+                    </td>
+                    <?php
+                } else {
+                    ?>
+                    <td>
+                        <span class="glyphicon glyphicon-check"></span> Downloaded<br>
+                        <form method="POST">
+                            <input type="hidden" name="version"
+                                   value="<?= htmlentities($update->version, ENT_COMPAT, 'UTF-8') ?>">
+                            <button type="submit" class="btn btn-primary" name="perform_update">
+                                Perform update
+                            </button>
+                        </form>
+                    </td>
+                    <?php
+                }
+                ?>
             </tr>
             <?php
         }
