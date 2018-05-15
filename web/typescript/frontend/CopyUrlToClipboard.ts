64fdc34ab235aa957362fa61335f289aefe86095
Tobias Hößl <tobias@hoessl.eu>
2018-05-15 14:45:45 +0200

    Fix clipboard name

--- web/typescript/frontend/CopyUrlToClipboard.ts
+++ web/typescript/frontend/CopyUrlToClipboard.ts
@@ -1,18 +1,18 @@
-declare let Clipboard: any;
+declare let ClipboardJS: any;
 
 export class CopyUrlToClipboard {
     constructor($widget: JQuery) {
         let $button = $widget.find("button"),
-            clipboard = new Clipboard($button[0]);
+            clipboard = new ClipboardJS($button[0]);
 
         $widget.find(".clipboard-done").addClass("hidden");
-        clipboard.on('success', function (e) {
+        clipboard.on('success', () => {
             $widget.find(".form-group").addClass("has-success has-feedback");
             $widget.find(".clipboard-done").removeClass("hidden");
             $button.focus();
         });
 
-        clipboard.on('error', function () {
+        clipboard.on('error', () => {
             alert("Could not copy the URL to the clipboard");
         });
     }
