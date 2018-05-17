cfd7d80503c6cf7d33f94544357d849cacc2e3d3
Tobias Hößl <tobias@hoessl.eu>
2018-05-17 10:13:08 +0200

    3.9.0a2

--- config/defines.php
+++ config/defines.php
@@ -6,6 +6,6 @@ use app\models\settings\AntragsgruenApp;
  * @var AntragsgruenApp $params
  */
 
-define('ANTRAGSGRUEN_VERSION', '3.9.0a1');
+define('ANTRAGSGRUEN_VERSION', '3.9.0a2');
 define('ANTRAGSGRUEN_HISTORY_URL', 'https://github.com/CatoTH/antragsgruen/blob/v3/History.md');
 define('ANTRAGSGRUEN_UPDATE_BASE', 'https://antragsgruen.de/updates/');
