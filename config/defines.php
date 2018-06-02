88b4dbd2852a3429f397377dbe87f80fe054ab4c
Tobias Hößl <tobias@hoessl.eu>
2018-06-02 11:55:09 +0200

    v3.9.0a6

--- config/defines.php
+++ config/defines.php
@@ -1,6 +1,6 @@
 <?php
 
-define('ANTRAGSGRUEN_VERSION', '3.9.0a5');
+define('ANTRAGSGRUEN_VERSION', '3.9.0a6');
 define('ANTRAGSGRUEN_HISTORY_URL', 'https://github.com/CatoTH/antragsgruen/blob/v3/History.md');
 define('ANTRAGSGRUEN_UPDATE_BASE', 'https://antragsgruen.de/updates/');
 define('ANTRAGSGRUEN_MIN_PHP_VERSION', '5.6.0');
