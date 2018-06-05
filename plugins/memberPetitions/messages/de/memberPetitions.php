77ce4567b9fec7979eb843b26c8cad1fbe5d6d85
Tobias Hößl <tobias@hoessl.eu>
2018-06-05 11:22:33 +0200

    rename messages files

---
+++ plugins/memberPetitions/messages/de/memberPetitions.php
@@ -0,0 +1,55 @@
+<?php
+
+return [
+    'title' => 'Beteiligungsgrün',
+    'bc'    => 'Beteiligungsgrün',
+
+    'index_create'       => 'Begehren anlegen',
+    'index_create_hint'  => 'Hiermit kannst du eine neue Petition anlegen. Wähle zunächst aus, an welchen Verband sich die Petition richtet:',
+    'index_orga_hint'    => 'Du bist Mitglied in folgenden Verbänden, die dieses Angebot nutzen:',
+    'index_my_petitions' => 'Meine Mitgliederbegehren',
+    'index_my_supports'  => 'Meine unterstützten Mitgliederbegehren',
+    'index_remaining'    => 'Verbleibend',
+
+    'sett_orgaid'            => 'Organisations-ID',
+    'sett_replydeadline'     => 'Antwort-Frist (Tage)',
+    'sett_title'             => 'Beteiligungsgrün',
+    'sett_minDiscussionTime' => 'Min. Diskussionszeit (Tage)',
+
+    'status_collecting'   => 'Unterstützer*innen sammeln',
+    'status_unanswered'   => 'Noch nicht beantwortet',
+    'status_answered'     => 'Beantwortet',
+    'status_discussing'   => 'Offene Diskussion',
+    'my_petitions'        => 'Meine Mitgliederbegehren',
+    'none'                => 'Keine',
+    'response_deadline'   => 'Beantwortungsfrist',
+    'response_overdue'    => 'abgelaufen',
+    'discussion_until'    => 'Diskussion bis',
+    'discussion_over'     => 'Die Diskussionsphase ist nun beendet. Du hast nun die Möglichkeit, die Rückmeldungen in deinen Antrag einzuarbeiten und dann Unterstützer*innen für dein Anliegen zu sammeln.',
+    'discussion_over_btn' => 'Rückmeldungen einarbeiten',
+
+    'policy_title'         => 'Organisations-Mitglieder',
+    'policy_desc'          => 'Mitglieder der Organisation dieser Veranstaltung',
+    'policy_motion_denied' => 'Nur Mitglieder des aktuellen Verbands können Petitionen anlegen.',
+    'policy_amend_denied'  => 'Nur Mitglieder des aktuellen Verbands können Änderungsanträge stellen.',
+    'policy_supp_denied'   => 'Nur Mitglieder des aktuellen Verbands können Petitionen unterstützen.',
+    'policy_comm_denied'   => 'Nur Mitglieder des aktuellen Verbands können kommentieren.',
+
+    'answer_hint'   => 'Dieses Mitgliederbegehren muss noch beantwortet werden. Die Funktion dafür befindet sich unten auf dieser Seite.',
+    'respond_title' => 'Das Mitgliederbegehren beantworten',
+    'respond_from'  => 'Antwort von',
+    'respond_btn'   => 'Beantworten',
+    'respond_saved' => 'Die Antwort wurde veröffentlicht.',
+
+    'response_title' => 'Antwort',
+    'response_from'  => 'Antwort von',
+    'response_date'  => 'Datum',
+    'response_text'  => 'Text',
+
+    'noti_response_title' => 'Dein Begehren wurden beantwortet',
+    'noti_response_text'  => "Hallo,\n\nherzlichen Dank für deine Geduld! Dein Begehren wurde soeben auf Beteiligungsgrün beantwortet. Du kannst diese Antwort hier einsehen:\n%LINK%\n\nMit freundlichen Grüßen,\n\nThomas Künstler",
+    'submit_discuss_title' => 'Diskussion gestartet',
+    'submit_discuss_text'  => "Hallo,\n\nherzlichen Dank für dein Engagement!\n\nDein Begehren wurde soeben auf Beteiligungsgrün veröffentlicht. Du kannst dieses hier einsehen: %LINK%\n\nDas Begehren steht nun zur Diskussion.\n\nMit freundlichen Grüßen,\n\nThomas Künstler",
+    'submit_petition_title' => 'Begehren eingereicht',
+    'submit_petition_text'  => "Hallo,\n\nherzlichen Dank für dein Engagement! Du hast dein Begehren soeben auf Beteiligungsgrün offiziell eingereicht. Du kannst dieses hier einsehen:\n%LINK%\n\nSobald der Vorstand eine Stellungnahme zu deinem Begehren abgegeben hat, informieren wir dich mit einer E-Mail.\n\nMit freundlichen Grüßen,\n\nThomas Künstler",
+];
