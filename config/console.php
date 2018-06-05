df1f7ad477f469577038fedb90e6d97574b23365
Tobias Hößl <tobias@hoessl.eu>
2018-06-05 09:53:49 +0200

    Fix Updater with custom config.json-locations / allow ANTRAGSGRUEN_CONFIG with ./yii

--- config/console.php
+++ config/console.php
@@ -5,7 +5,16 @@ $configDir = __DIR__ . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'model
 require_once($configDir . 'JsonConfigTrait.php');
 require_once($configDir . 'AntragsgruenApp.php');
 
-$config = file_get_contents(__DIR__ . DIRECTORY_SEPARATOR . 'config.json');
+
+if (isset($_SERVER['ANTRAGSGRUEN_CONFIG'])) {
+    $configFile = $_SERVER['ANTRAGSGRUEN_CONFIG'];
+} elseif (isset($_ENV['ANTRAGSGRUEN_CONFIG'])) {
+    $configFile = $_SERVER['ANTRAGSGRUEN_CONFIG'];
+} else {
+    $configFile = __DIR__ . DIRECTORY_SEPARATOR . 'config.json';
+}
+
+$config = file_get_contents($configFile);
 $params = new \app\models\settings\AntragsgruenApp($config);
 $common = require(__DIR__ . DIRECTORY_SEPARATOR . 'common.php');
 unset($common['defaultRoute']);
