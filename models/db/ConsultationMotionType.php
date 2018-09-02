3d329b4bff52b0a74b4e1508f93476577bf4a8f6
joriki <joriki27@gmail.com>
2018-09-02 15:45:53 +0200

    fixed typo in previous commit

--- models/db/ConsultationMotionType.php
+++ models/db/ConsultationMotionType.php
@@ -442,7 +442,7 @@ class ConsultationMotionType extends ActiveRecord
     {
         $return = [];
         foreach ($this->motions as $motion) {
-            if (!in_array($motion->status, $this->getConsultation()->getInvisibleMotionyStatuses(!$includeWithdrawn))) {
+            if (!in_array($motion->status, $this->getConsultation()->getInvisibleMotionStatuses(!$includeWithdrawn))) {
                 $return[] = $motion;
             }
         }
