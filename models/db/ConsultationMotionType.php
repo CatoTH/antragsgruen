34d99f634443b233f94efb50d64dc4e0540ce491
Tobias Hößl <tobias@hoessl.eu>
2018-09-15 14:44:22 +0200

    PDF Fixes

--- models/db/ConsultationMotionType.php
+++ models/db/ConsultationMotionType.php
@@ -415,7 +415,6 @@ class ConsultationMotionType extends ActiveRecord
 
     /**
      * @return \app\models\settings\MotionType
-     * @throws \Exception
      */
     public function getSettingsObj()
     {
