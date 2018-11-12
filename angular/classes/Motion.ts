4ec6b4c9f5e40119c2d3e9fbd56f8b1d8367e1e5
Tobias Hößl <tobias@hoessl.eu>
2018-11-12 21:03:51 +0100

    Sorting / links

--- angular/classes/Motion.ts
+++ angular/classes/Motion.ts
@@ -16,12 +16,12 @@ export class Motion extends IMotion {
     }
 
     public getLink(linkKey: string, linkTemplates: { [key: string]: string }): string {
-        let template = linkTemplates[linkKey];
+        const template = linkTemplates[linkKey];
         if (!template) {
             console.warn('Unknown link key:', linkKey);
             return '';
         }
-        let slug = (this.slug ? this.slug : this.id);
+        const slug = (this.slug ? this.slug : this.id);
         return template
             .replace(/0123456789/, this.id)
             .replace(/_SLUG_/, slug)
