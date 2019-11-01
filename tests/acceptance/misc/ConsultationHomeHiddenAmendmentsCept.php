f4dbaa54d64cb5f3425b2cec749d0bad1f8c9596
Tobias Hößl <tobias@hoessl.eu>
2019-11-01 14:18:08 +0100

    Add test dummy

---
+++ tests/acceptance/misc/ConsultationHomeHiddenAmendmentsCept.php
@@ -0,0 +1,13 @@
+<?php
+
+/** @var \Codeception\Scenario $scenario */
+
+$scenario->incomplete('not yet implemented');
+
+/*
+$I = new AcceptanceTester($scenario);
+$I->populateDBData1();
+
+- Activate the long home page view, with hidden amendments
+
+*/
