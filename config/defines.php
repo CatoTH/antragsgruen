510a390836235ad9665f501fc27c33c67a59418e
Tobias Hößl <tobias@hoessl.eu>
2020-02-04 15:20:12 +0100

    4.5.0-a3

--- config/defines.php
+++ config/defines.php
@@ -1,6 +1,6 @@
 <?php
 
-define('ANTRAGSGRUEN_VERSION', '4.5.0-a2');
+define('ANTRAGSGRUEN_VERSION', '4.5.0-a3');
 define('ANTRAGSGRUEN_HISTORY_URL', 'https://github.com/CatoTH/antragsgruen/blob/master/History.md');
 define('ANTRAGSGRUEN_UPDATE_BASE', 'https://antragsgruen.de/updates/');
 define('ANTRAGSGRUEN_MIN_PHP_VERSION', '7.1.0');
