077ce539b335a3c799ffa6b0e274a327c31b7104
Tobias Hößl <tobias@hoessl.eu>
2020-06-10 21:02:48 +0200

    Bugfix: amendment prefixes could break everything

--- models/amendmentNumbering/IAmendmentNumbering.php
+++ models/amendmentNumbering/IAmendmentNumbering.php
@@ -65,7 +65,7 @@ abstract class IAmendmentNumbering
                 $spl    = explode($split, $prefix);
                 $prefix = $spl[0];
             }
-            $number = preg_replace('/^(.*[^0-9])?([0-9]+)([^0-9]*)$/siu', '$2', $prefix);
+            $number = intval(preg_replace('/^(.*[^0-9])?([0-9]+)([^0-9]*)$/siu', '$2', $prefix));
             if ($number > $maxRev) {
                 $maxRev = $number;
             }
