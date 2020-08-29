375f67aa2c84ba012104e8c849a091e182b5f1c0
Tobias Hößl <tobias@hoessl.eu>
2020-08-29 08:19:58 +0200

    Update libraries

--- web/js/build/frontend/ContentPageCreate.js
+++ web/js/build/frontend/ContentPageCreate.js
@@ -1,2 +1,2 @@
-define(["require","exports"],function(e,r){"use strict";Object.defineProperty(r,"__esModule",{value:!0});var n=function(e){this.$form=e,this.$form.find("input[name=url]").on("keyup change keypress",function(e){var r=$(e.currentTarget);r.val(r.val().replace(/[^\w_\-,\.äöüß]/g,""))})};r.ContentPageCreate=n});
+define(["require","exports"],function(e,t){"use strict";Object.defineProperty(t,"__esModule",{value:!0}),t.ContentPageCreate=void 0;var n=function(e){this.$form=e,this.$form.find("input[name=url]").on("keyup change keypress",function(e){var t=$(e.currentTarget);t.val(t.val().replace(/[^\w_\-,\.äöüß]/g,""))})};t.ContentPageCreate=n});
 //# sourceMappingURL=ContentPageCreate.js.map
