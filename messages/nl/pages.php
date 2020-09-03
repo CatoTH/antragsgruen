291eae31b3deac70bd9dc0610ed3adb6b63a3c96
Martijn <martijn@mrtijn.nl>
2020-09-03 21:08:35 +0200

    Add Dutch translation for pages.php

---
+++ messages/nl/pages.php
@@ -0,0 +1,52 @@
+<?php
+
+return [
+    'list_title'      => 'Pagina's',
+    'list_add_std'    => 'Maak een standaardpagina',
+    'list_add_custom' => 'Maak een nieuwe pagina',
+    'list_edit'       => 'Pas pagina's aan',
+    'list_all_cons'   => 'Alle consultaties',
+    'list_this_con'   => 'Alleen deze consultatie',
+
+    'content_privacy_title' => 'Privacyverklaring',
+    'content_privacy_bread' => 'Privacy',
+    'content_welcome'       => 'Welkom',
+    'content_welcome_text'  => '<p>Welkom bij Antragsgrün</p>',
+    'content_imprint_title' => 'Contactgegevens',
+    'content_imprint_bread' => 'Contactgegevens',
+    'content_help_title'    => 'Help',
+    'content_help_bread'    => 'Help',
+    'content_help_place'    => '<p>Help...</p>',
+    'content_maint_bread'   => 'Onderhoud',
+    'content_maint_title'   => 'Onderhoudsmodus',
+    'content_maint_text'    => '<p>Deze consultatie is (nog) niet publiek gemaakt.</p>',
+    'content_login_pre'     => 'Inloggen (boven)',
+    'content_login_post'    => 'Inloggen (beneden)',
+    'content_feeds_title'   => 'RSS-Feeds',
+    'content_feeds_text'    => '<p>RSS-feeds zijn een handige manier om op de hoogte te blijven van veel verschillende nieuwsbronnen..</p>
+        <p>Je gebruikt een RSS-reader programma om te abonneren op allerlei nieuwssites, blogs, webcomics, of Antragsgrün-sites die je interessant vindt.</p>
+        <p>Dan laat het programma je een overzicht zien van alle nieuwe artiekelen op deze sites.</p>
+        <p>Hier zijn een aantal programmas en webdiensten om RSS feeds te lezen:</p>
+        <ul>
+            <li><a href="http://www.rssowl.org/">RSSOwl</a> (Linux, macOS, Windows)</li>
+            <li><a href="https://ranchero.com/netnewswire/">NetNewsWire</a> (macOS)</li>
+            <li><a href="https://reederapp.com/">Reeder</a> (macOS, iOS)</li>
+            <li><a href="https://www.vienna-rss.com/">Vienna</a> (macOS)</li>
+            <li><a href="https://www.microsoft.com/en-us/p/newsflow/9nblggh58s5r">Newsflow</a> (Windows)</li>
+            <li><a href="https://feedly.com/">Feedly</a> (Web-Dienst)</li>
+            <li><a href="https://feedbin.com/">Feedbin</a> (Web-Dienst)</li>
+        </ul>',
+
+    'images_browse_title' => 'Bekijk de afbeeldingen die voor deze consultatie zijn geüpload',
+    'images_none'         => 'Er zijn nog geen afbeeldingen geüpload.',
+    'images_delete'       => 'Verwijderen',
+    'images_deleted'      => 'Deze afbeelding is verwijderd.',
+
+    'settings_allcons' => 'Websitebreed',
+    'settings_inmenu'  => 'In menu laten zien',
+    'settings_url'     => 'URL',
+    'settings_title'   => 'Titel',
+    'settings_delete'  => 'Verwijder pagina',
+    'create_btn'       => 'Toevoegen',
+    'menu_add_btn'     => 'Voeg een nieuwe pagina toe aan het menu',
+];
