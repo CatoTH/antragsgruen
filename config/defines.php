30b44ad4b1754d7e9b9e9c4021643ed2ea568c28
Tobias Hößl <tobias@hoessl.eu>
2020-11-29 15:07:57 +0100

    Hint about PHP minimum version

--- config/defines.php
+++ config/defines.php
@@ -4,5 +4,5 @@ define('ANTRAGSGRUEN_VERSION', '4.6.2');
 define('ANTRAGSGRUEN_HISTORY_URL', 'https://github.com/CatoTH/antragsgruen/blob/master/History.md');
 define('ANTRAGSGRUEN_UPDATE_BASE', 'https://antragsgruen.de/updates/');
 define('ANTRAGSGRUEN_MIN_PHP_VERSION', '7.1.0');
-define('ANTRAGSGRUEN_NEXT_PHP_VERSION', '7.1.0');
+define('ANTRAGSGRUEN_NEXT_PHP_VERSION', '7.2.0');
 define('ANTRAGSGRUEN_INSTALLATION_SOURCE', 'repository-source');
