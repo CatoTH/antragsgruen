a9c2c2d7bfb1e07bd6c566870506df87ba9e86a6
Tobias Hößl <tobias@hoessl.eu>
2020-12-31 18:21:48 +0100

    Finish first test case

--- tests/acceptance/speech/QuotaLoggedInCept.php
+++ tests/acceptance/speech/QuotaLoggedInCept.php
@@ -52,11 +52,48 @@ $I->see('Offener Platz', '.subqueues');
 $I->executeJS('var chkbox = document.querySelector(".toolbarBelowTitle.settings .settingsOpen input"), evt = document.createEvent("HTMLEvents"); chkbox.checked = true; evt.initEvent("change", false, true); chkbox.dispatchEvent(evt);');
 $I->wait(0.1);
 
-// Applying is now possible
+// Applying / withdrawing is now possible
 $I->gotoConsultationHome();
 $I->seeElement('.currentSpeechInline');
+$I->dontSeeElement('.currentSpeechInline .appliedMe');
 $I->see('Redeliste', '.currentSpeechInline');
+$I->see('0', '.currentSpeechInline .number');
 $I->dontSeeElement('.currentSpeechInline .notPossible');
 $I->see('Bewerben', '.waitingSubqueues .applied button');
-$I->executeJS('var chkbox = document.querySelector(".waitingSubqueues .applied button"), evt = document.createEvent("HTMLEvents"); evt.initEvent("click", false, true); chkbox.dispatchEvent(evt);');
+$I->executeJS('var btn = document.querySelectorAll(".waitingSubqueues .applied button").item(1), evt = document.createEvent("HTMLEvents"); evt.initEvent("click", false, true); btn.dispatchEvent(evt);');
 $I->wait(0.1);
+$I->seeInField('#speechRegisterName2', 'Testadmin');
+$I->executeJS('var form = document.querySelectorAll(".waitingSubqueues form").item(0), evt = document.createEvent("HTMLEvents"); evt.initEvent("submit", false, true); form.dispatchEvent(evt);');
+$I->wait(0.1);
+$I->seeElement('.currentSpeechInline .appliedMe');
+$I->see('1', '.currentSpeechInline .number');
+$I->see('Testadmin', '.currentSpeechInline .nameList');
+$I->executeJS('var btn = document.querySelectorAll(".waitingSubqueues .btnWithdraw").item(0), evt = document.createEvent("HTMLEvents"); evt.initEvent("click", false, true); btn.dispatchEvent(evt);');
+$I->see('0', '.currentSpeechInline .number');
+$I->dontSee('Testadmin', '.currentSpeechInline .nameList');
+
+// Testing administration functions
+$I->click('.currentSpeechInline .speechAdminLink');
+
+$I->dontSeeElement('.subqueueAdder form');
+$I->executeJS('var btn = document.querySelectorAll(".subqueues .adderOpener").item(0), evt = document.createEvent("HTMLEvents"); evt.initEvent("click", false, true); btn.dispatchEvent(evt);');
+$I->wait(0.1);
+$I->seeElement('.subqueueAdder form');
+$I->fillField('#subqueueAdderName1', 'Testperson');
+$I->executeJS('var btn = document.querySelectorAll(".subqueues .subqueueAdder form").item(0), evt = document.createEvent("HTMLEvents"); evt.initEvent("submit", false, true); btn.dispatchEvent(evt);');
+$I->wait(0.1);
+
+$I->see('Testperson', '.slotPlaceholder.active');
+$I->seeElement('.slotActive.inactive');
+
+$I->executeJS('var btn = document.querySelectorAll(".slotPlaceholder.active").item(0), evt = document.createEvent("HTMLEvents"); evt.initEvent("click", false, true); btn.dispatchEvent(evt);');
+$I->wait(0.2);
+
+$I->see('Testperson', '.slotEntry.slotActive');
+$I->seeElement('.slotPlaceholder.inactive');
+
+$I->gotoConsultationHome();
+$I->see('Testperson', '.currentSpeechInline .activeSpeaker');
+
+$I->gotoMotion();
+$I->see('Testperson', '.currentSpeechFooter .activeSpeaker');
