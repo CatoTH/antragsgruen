a92324df242ae1021a674bdc370dfecc30dabf86
Tobias Hößl <tobias@hoessl.eu>
2021-04-27 21:52:03 +0200

    Sort motions differently

--- views/admin/motion-list/motion_list.php
+++ views/admin/motion-list/motion_list.php
@@ -20,6 +20,7 @@ $layout->addBreadcrumb(Yii::t('admin', 'bread_list'));
 echo '<h1>' . Yii::t('admin', 'list_head_title') . '</h1>';
 echo '<div class="content">';
 
+$motionStatuses = $consultation->getStatuses()->getStatusNames();
 /** @var Motion[] $motionsVisible */
 $motionsVisible = [];
 /** @var Motion[] $motionsInvisible */
@@ -33,16 +34,20 @@ foreach ($motions as $motion) {
     }
 }
 
-echo '<h2>' . Yii::t('admin', 'list_visibles') . '</h2>';
+echo '<h2>' . Yii::t('admin', 'list_invisibles') . '</h2>';
 
 foreach ($motionsInvisible as $motion) {
     $url = UrlHelper::createUrl(['admin/motion-list/index', 'motionId' => $motion->id]);
     echo Html::a('<span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span> ' . Html::encode($motion->getTitleWithPrefix()), $url);
-    echo " <small>(" . $motion->getFormattedStatus() . ")</small>";
-    echo "<br>";
+
+    echo ' <small>' . Html::encode($motionStatuses[$motion->status]);
+    if ($motion->status === Motion::STATUS_COLLECTING_SUPPORTERS) {
+        echo ' (' . count($motion->getSupporters(true)) . ')';
+    }
+    echo '</small><br>';
 }
 
-echo '<h2>' . Yii::t('admin', 'list_invisibles') . '</h2>';
+echo '<h2>' . Yii::t('admin', 'list_visibles') . '</h2>';
 
 foreach ($motionsVisible as $entry) {
     $url = UrlHelper::createUrl(['admin/motion-list/index', 'motionId' => $entry->id]);
