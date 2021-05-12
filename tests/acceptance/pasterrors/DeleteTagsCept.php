f2b4eb17d027e6969c75de1156ff49fe56344348
Tobias Hößl <tobias@hoessl.eu>
2021-05-12 21:01:19 +0200

    Fix test case

--- tests/acceptance/pasterrors/DeleteTagsCept.php
+++ tests/acceptance/pasterrors/DeleteTagsCept.php
@@ -6,9 +6,11 @@ $I->populateDBData1();
 
 $I->gotoConsultationHome();
 $page = $I->loginAndGotoStdAdminPage()->gotoConsultation();
-$I->executeJS("$('#tagsList').pillbox('removeByText', 'Verkehr');");
+$I->wait(0.5);
+$I->executeJS('document.querySelector("#tagsList select").selectize.removeItem("Verkehr");');
 $page->saveForm();
+$I->wait(0.5);
 $I->seeElement('#consultationSettingsForm');
-if ($I->executeJS('return $("#tagsList").pillbox("items").length') !== 2) {
+if ($I->executeJS('return document.querySelector("#tagsList select").selectize.items.length') != 2) {
     $I->fail('Invalid return from tag-List');
 }
