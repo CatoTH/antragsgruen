83d0102240be083a1a1e38a8db5194fbeb09f7e7
Tobias Hößl <tobias@hoessl.eu>
2021-10-29 21:26:26 -0400

    Plugin: add specific logo

--- plugins/egp/pdf/Egp.php
+++ plugins/egp/pdf/Egp.php
@@ -12,7 +12,11 @@ class Egp extends IPDFLayout
 
     protected function setHeaderLogo(Consultation $consultation, int $abs, ?float $maxWidth, ?float $maxHeight)
     {
-        if ($consultation->urlPath === 'spring2021_council') {
+        if ($consultation->urlPath === 'winter2021_council') {
+            $logo = file_get_contents(__DIR__ . '/../assets/council-2021-12.png');
+            $width = 678;
+            $height = 298;
+        } elseif ($consultation->urlPath === 'spring2021_council') {
             $logo = file_get_contents(__DIR__ . '/../assets/council-2021-05.png');
             $width = 678;
             $height = 298;
