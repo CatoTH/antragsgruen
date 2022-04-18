fba14f6a31d6dca0907305af27662f94f4446439
Tobias Hößl <tobias@hoessl.eu>
2022-04-18 16:52:56 +0200

    4.10.0-beta

--- config/defines.php
+++ config/defines.php
@@ -1,6 +1,6 @@
 <?php
 
-define('ANTRAGSGRUEN_VERSION', '4.10.0-alpha');
+define('ANTRAGSGRUEN_VERSION', '4.10.0-beta');
 define('ANTRAGSGRUEN_HISTORY_URL', 'https://github.com/CatoTH/antragsgruen/blob/main/History.md');
 define('ANTRAGSGRUEN_UPDATE_BASE', 'https://antragsgruen.de/updates/');
 define('ANTRAGSGRUEN_MIN_PHP_VERSION', '7.3.0');
