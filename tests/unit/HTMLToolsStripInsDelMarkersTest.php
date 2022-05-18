92cf91fe709ec37e66d2f05af15b0da13435d3e8
Tobias Hößl <tobias@hoessl.eu>
2022-05-18 20:45:59 +0200

    Bugfix: more than 26 latin characters

--- tests/unit/HTMLToolsStripInsDelMarkersTest.php
+++ tests/unit/HTMLToolsStripInsDelMarkersTest.php
@@ -9,9 +9,7 @@ class HTMLToolsStripInsDelMarkersTest extends TestBase
 {
     use Specify;
 
-    /**
-     */
-    public function testInsDel()
+    public function testInsDel(): void
     {
         $orig     = '<strong>Test <ins>Inserted <em>EM</em></ins><del>old</del></strong><br>';
         $expected = '<strong>Test Inserted <em>EM</em></strong><br>';
@@ -19,9 +17,7 @@ class HTMLToolsStripInsDelMarkersTest extends TestBase
         $this->assertEquals($expected, $out);
     }
 
-    /**
-     */
-    public function testClasses()
+    public function testClasses(): void
     {
         $orig     = '<ul><li>Test 1</li><li class="inserted">test <strong>2</strong></li><li class="deleted">Test 3</li></ul><div class="inserted underlined">Another line</div>';
         $expected = '<ul><li>Test 1</li><li>test <strong>2</strong></li></ul><div class="underlined">Another line</div>';
