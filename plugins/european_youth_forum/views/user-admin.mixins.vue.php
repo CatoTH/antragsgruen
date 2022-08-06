94b60e68ea240f104ef78c9939a704422e3ea2a4
Tobias Hößl <tobias@hoessl.eu>
2022-08-06 22:17:36 +0200

    First mockup of voting round administration

--- plugins/european_youth_forum/views/user-admin.mixins.vue.php
+++ plugins/european_youth_forum/views/user-admin.mixins.vue.php
@@ -4,9 +4,62 @@
     }
 
     window.USER_ADMIN_MIXINS.push({
+        data() {
+            return {
+                yfjSelectedVotingRound: null,
+            }
+        },
+        computed: {
+            yfjVotingRounds: function () {
+                let rounds = [];
+                this.sortedGroups.forEach(group => {
+                    if (!group.title.match(/Voting \d+:/)) {
+                        return;
+                    }
+                    const votingNumber = group.title.split(':')[0].split(' ')[1];
+                    if (rounds.indexOf(votingNumber) === -1) {
+                        rounds.push(votingNumber);
+                    }
+                });
+                return rounds;
+            },
+            yfjNycUsersInSelectedVotingRound: function () {
+                let groups = this.groups.filter(group => {
+                    return group.title.indexOf('NYC') !== -1;
+                });
+                return this.yfjUsersInGroups(groups);
+            },
+            yfjIngyoUsersInSelectedVotingRound: function () {
+                let groups = this.groups.filter(group => {
+                    return group.title.indexOf('INGYO') !== -1;
+                });
+                return this.yfjUsersInGroups(groups);
+            },
+        },
         methods: {
-            test: function () {
-                return 'Bla';
+            yfjChooseVotingRound: function (round) {
+                this.yfjSelectedVotingRound = round;
+            },
+            yfjResetVotingRound: function () {
+                this.yfjSelectedVotingRound = null;
+            },
+            yfjUsersInGroups: function (groups) {
+                return this.users.filter(user => {
+                    let found = false;
+                    groups.forEach(group => {
+                        if (user.groups.indexOf(group.id) !== -1) {
+                            found = true;
+                        }
+                    });
+                    return found;
+                });
+            },
+            yfjHasVotingRights: function (user, pillar) {
+                const group = this.groups.find(group => group.title === "Voting " + this.yfjSelectedVotingRound + ": " + pillar);
+                if (!group) {
+                    return false;
+                }
+                return user.groups.indexOf(group.id) !== -1;
             }
         }
     });
