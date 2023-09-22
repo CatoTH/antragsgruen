6293ec19dd4f74004d10ccb714b36ea2243af8e2
Tobias Hößl <tobias@hoessl.eu>
2023-09-22 16:36:58 +0200

    Experiment with gruenes CI 2023

---
+++ plugins/gruen_ci/Assets3.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace app\plugins\gruen_ci;
+
+use yii\web\AssetBundle;
+
+class Assets3 extends AssetBundle
+{
+    public $sourcePath = '@app/plugins/gruen_ci/assets/';
+
+    public $css = [
+        'layout-gruen_ci3.css',
+    ];
+    public $js = [
+    ];
+    public $depends = [
+    ];
+}
