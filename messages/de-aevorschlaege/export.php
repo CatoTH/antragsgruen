14e2f35bd3e245d9d8a7b6eca0e5559d55cbeb14
Tobias Hößl <tobias@hoessl.eu>
2023-10-22 10:52:00 +0200

    Wording bugfix

--- messages/de-aevorschlaege/export.php
+++ messages/de-aevorschlaege/export.php
@@ -3,8 +3,8 @@
 return [
     'amendment'             => 'Änderungsvorschlag',
     'mail_amendment_x_to_y' => 'Änderungsvorschlags %AMENDMENT% zum Antrag %MOTION%',
-    'all_amendments_title'  => 'Alle Änderungsvorschläg',
-    'amendments'            => 'Änderungsvorschläg',
-    'btn_amendments'        => 'Änderungsvorschläg',
-    'pdf_merging_init'      => 'Ausgewählte Änderungsvorschläg',
+    'all_amendments_title'  => 'Alle Änderungsvorschläge',
+    'amendments'            => 'Änderungsvorschläge',
+    'btn_amendments'        => 'Änderungsvorschläge',
+    'pdf_merging_init'      => 'Ausgewählte Änderungsvorschläge',
 ];
