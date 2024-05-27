2d3d25d7905fead2b7dde9918eb27c16fc3e67d6
Tobias Hößl <tobias@hoessl.eu>
2024-05-27 12:11:16 +0200

    More help on creating amendments

--- plugins/antragsgruen_sites/views/manager/help_amendments_de.php
+++ plugins/antragsgruen_sites/views/manager/help_amendments_de.php
@@ -32,7 +32,7 @@ $params = \app\models\settings\AntragsgruenApp::getInstance();
         </li>
     </ul>
 
-    <p><strong>Einleitung</strong></p>
+    <h2 id="intro">Einleitung</h2>
 
     <p>In dieser Anleitung zeigen wir zunächst, wie es aus Sicht einer Nutzer*in aussieht, einen Antrag zu stellen. Im zweiten Teil stellen wir dann verschiedene Möglichkeiten vor, als Administrator*in diesen Vorgang an die Bedürfnisse der jeweiligen Organisation anzupassen.</p>
 
@@ -62,5 +62,52 @@ $params = \app\models\settings\AntragsgruenApp::getInstance();
 
     <p>Auf diese Weise kann man als Delegierte einfach den ursprünglichen Antrag lesen, sieht aber auch auf den ersten Blick, welche Stellen gegebenenfalls umstritten ist, und welche vorgeschlagenen Änderungen es gibt.</p>
 
-    <p>MEHR FOLGT...</p>
+    <h2 id="setup">Einrichtung</h2>
+
+    <p>Die Voraussetzung, um Änderungsanträge stellen zu können, ist natürlich ein änderbares Basisdokument. Dabei kann es sich entweder um einen Antrag eines anderen Mitglieds handeln, oder aber um einen Textvorschlag eines administrativen Gremiums, z.B. den Entwurf eines Wahlprogramms, das von einer Programmkommission erarbeitet wurde.</p>
+
+    <p>Es gibt noch eine dritte Variante: bei Satzungsänderungen gibt es auch ein Basisdokument (die Satzung), das aber nicht als reguläres zu beschließendes Dokument auf der Seite erscheint - vielmehr haben hier Satzungsänderungsanträge optisch den selben Rang wie eigenständige Anträge. Hierzu wird es aber ein separates Tutorial geben.</p>
+
+    <p>Wie Mitglieder (änderbare) Anträge einreichen können, wird in einem <a href="/help/member-motion">separaten Tutorial</a> erklärt. Werden die Basisdokumente hingegen nur von Administrierenden eingestellt (das heißt, in den Berechtigungen zum Anlegen von Anträgen ist „Niemand“ oder „Admins“ gesetzt), befindet sich die Funktion zum Anlegen von Anträgen in der Admin-Antragsliste: man geht im Menü auf Antragsliste (dieser Punkt erscheint nur, wenn man administrative Rechte hat), und wählt dann in der Leiste oben „Neu“ -> „Antrag“ (bzw. den Namen des jeweiligen Antragstyps). Hier im Admin-Bereich stehen grundsätzlich immer alle Antragstypen zur Auswahl, unabhängig von den gesetzten Berechtigungen.</p>
+
+    <p><strong>Hinweis zu längeren Texten</strong>: häufig werden auch längere Textentwürfe auf Antragsgrün veröffentlicht - beispielsweise komplette Wahlprogramme. Es empfiehlt sich hier dringend, nicht den kompletten Text als ein einziges Dokument zu veröffentlichen, sondern unterteilt in mehrere Einzeldokumente. Beispielsweise ein Dokument pro Kapitel. Hintergrund dieser Empfehlung ist, dass der Rechenaufwand, den Antragsgrün auf dem Server erzeugt, sowohl mit der Länge des Basistexts als auch der Anzahl der Änderungsanträge pro Basistext steigt. Zehn Kapitel mit je 20 Änderungsanträgen sind viel einfacher handzuhaben als ein Dokument mit 200 Änderungsanträgen!</p>
+
+    <p>Hinweis zu <strong>selbst zusammengestellten Antrags-Vorlagen</strong>: stellt man sich einen neuen Antragstypen mit verschiedenen Antrags-Abschnitten zusammen, ist zu beachten, dass nur Abschnitte des einfachen Typs „Text“ sowie „Titel“ änderbar sind - und auch nur dann, wenn das Häkchen bei „In Änderungsanträgen“ gesetzt ist. Angehängte PDFs oder Bilder sind beispielsweise nicht im Rahmen von Änderungsanträgen änderbar, und auch die Begründungen in Anträgen sind (sofern nicht anders eingestellt) nicht Gegenstand von Änderungsanträgen. Es lassen sich aber ohne weiteres mehrere Textfelder anlegen, die änderbar sind, oder eben das „In Änderungsanträgen“-Häkchen bei der Begründung setzen, falls diese auch geändert werden können sollen.</p>
+
+    <h3 id="permissions">Berechtigungen</h3>
+
+    <p>Die Berechtigungen zum Anlegen von Änderungsanträgen können auf die selbe Weise konfiguriert werden wie zum Anlegen von Anträgen. Das heißt, als Teil des Antragstypen unter „Berechtigungen“ -> „Änderungsanträge anlegen“. Eine genauere Beschreibung der Auswahlmöglichkeiten gibt es im <a href="/help/member-motion">Tutorial zum Anlegen von Anträgen</a>.</p>
+
+    <h3 id="singleparagraph">Beschränkung auf einen Absatz</h3>
+
+    <p>Es gibt bei Änderungsanträgen eine zusätzliche Auswahlmöglichkeit: „Änderungsanträge dürfen sich nur auf einen Absatz beziehen“. Wie der Name bereits andeutet, lässt sich hiermit steuern, ob Mitglieder in einem Änderungsantrag beliebig viele Stellen des Antrags ändern können oder nicht. Welche Variante zu bevorzugen ist hängt stark von der inhaltlichen Dynamik der jeweiligen Organisation ab:</p>
+    <ul>
+        <li>Nur ein Absatz darf geändert werden: diese Option wird typischerweise gewählt, wenn eher nur kleinere, lokale Änderungen gewünscht sind, bzw. Mitglieder dazu angehalten werden, für inhaltlich unterschiedliche „Änderungs-Gedanken“ auch unterschiedliche Änderungsanträge zu stellen.</li>
+        <li>Beliebig viele Stellen dürfen geändert werden: ohne diese Beschränkung haben Mitglieder mehr Flexibilität darin, auch komplexere Änderungen am Text vorzuschlagen.</li>
+    </ul>
+
+    <p>Wird die Antragstellung auf einen Absatz beschränkt, gibt es noch die darüber hinaus gehende Option „..und nur auf eine konkrete Stelle“, die Änderungen auch innerhalb eines Absatzes auf eine einzige Stelle beschränkt. Diese Möglichkeit beschränkt unserer Erfahrung nach Mitglieder aber oft zu sehr, wir empfehlen sie daher nur dann, wenn ausschließlich Änderungen beispielsweise an konkreten Zahlenwerten erwünscht sind.</p>
+
+    <p>Werden Änderungsanträge auf einen Absatz beschränkt, wird das Stellen eines Änderungsantrags für Mitglieder in einer Hinsicht noch erleichtert: es erscheinen dann (auf hinreichen großen Bildschirmen, nicht auf Smartphones) in der regulären Lese-Ansicht des Antrags rechts von jedem Absatz eine Änderungs-Link, wie in der ersten Abbildung oben auch zu sehen ist.</p>
+
+    <h3 id="globalalternatives">Global-Alternativen, Redaktionelle Änderungsanträge</h3>
+
+    <p>Es gibt zwei spezielle Arten von Änderungsanträgen, die aber erst explizit von den Administrierenden aktiviert werden müssen und es den Mitgliedern erlauben, etwas aus dem regulären Ablauf der Änderungsanträge auszubrechen. Beide haben den Zweck, die spätere Beschlusserstellung zu vereinfachen, falls ein Änderungsantrag sich im regulären Ablauf auf zu viele Stellen beziehen würde.</p>
+
+    <p>Mit „Globalalternative“ können Mitglieder (oder Admins, nachträglich) Änderungsanträge markieren, die nicht nur einzelne Aspekte des Antrags ändern, sondern den Antrag gewissermaßen komplett ersetzen. In der Praxis wird ein solcher Änderungsantrag zunächst aus einer Streichung des kompletten bisherigen Texts bestehen, gefolgt von der Einfügung des neuen Texts. Bei der Eingabe der Änderung ändert sich durch dieses Häkchen zunächst noch nichts, dafür aber bei der Anzeige: eine solche Globalalternative wird nicht bei jedem betroffenen Absatz einzeln angezeigt (jeder einzelne Absatz wäre durch die Streichung betroffen), sondern nur einmal unterhalb des Antrags, als markiert als Globalalernative. Auch wenn später die Änderungsanträge eingepflegt werden, wird gar nicht erst versucht werden, einen solchen Änderungsantrag mit anderen Änderungsanträgen zusammenzuführen, da dies in der Praxis nie möglich sein wird.<br>
+        Hinweis: Globalalternativen sind nicht möglich, wenn gleichzeitig die Option "Änderungsanträge dürfen sich nur auf einen Absatz beziehen“ ausgewählt ist.</p>
+
+    <p>Redaktionelle Änderungsanträge beziehen sich auf Änderungsanträge, die sich eher auf stilistische oder strukturelle Aspekte des Basistexts beziehen. Sie werden nicht dadurch gestellt, dass das antragsteilende Mitglied jede gewünschte Änderung einzeln vornimmt, sondern in Form einer Anweisung / Bitte an die Redaktion. Ein Beispiel wäre ein Antrag, die Leser*in des Antrags lieber zu Sietzen statt zu Duzen. Würde das Mitglied hier jedes Vorkommen von „Du“ händisch ersetzen, würde der Änderungsantrag beim Lesen des Basis-Antrags an nahezu jedem Absatz erneut auftauchen, und beim Einpflegen der Änderungsanträge möglicherweise zu Konflikten mit anderen Änderungsanträgen führen. Stattdessen wird das tatsächliche Ändern, falls der Änderungsantrag übernommen werden soll, am Ende der Beschlussfassung händisch von der Redaktion vorgenommen.</p>
+
+    <p>Beide Varianten lassen sich in den Veranstaltungs-Einstellungen (de-)aktivieren (also ausnahmsweise nicht beim Antragstypen selbst). Konkret unter „Einstellungen“ -> „Diese Veranstaltung / Programmdiskussion“ -> „Änderungsanträge“, und dann:</p>
+    <ul>
+        <li>„Redaktionelle Änderungsanträge zulassen“ bzw.</li>
+        <li>„Globalalternativen zulassen“</li>
+    </ul>
+
+    <h3 id="supporters">Wer stellt den Änderungsantrag & Unterstützer*innen sammeln</h3>
+
+    <p>Für Änderungsanträge gibt es die selben Möglichkeiten wie bei den Anträgen, sowohl die abgefragten Daten zur Antragstellerin anzupassen, als auch Unterstützer*innen abzufragen, ggf. durch eine explizite vorgeschaltete Unterstützungs-Phase. Hierzu wird auf das <a href="/help/member-motion">Tutorial zum Anlegen von Anträgen</a> verwiesen.</p>
+
+    <p>Es ist allerdings zu beachten, dass die Einstellungen standardmäßig sowohl für das Stellen von Anträgen als auch von Änderungsanträgen gelten. Wird eine Unterstützungs-Phase eingestellt, gilt dies also sowohl wenn man einen Antrag stellt, als auch wenn man einen Änderungsantrag stellt. Will man hier ein unterschiedliches Prozedere, muss zunächst bei den Einstellungen für die Antragsteller*innen/Unterstützer*innen das Häkchen bei „Die selben Einstellungen auch für Änderungsanträge“ entfernt werden. Dann erscheint der selbe Einstellungsblock ein weiteres Mal, nur dass nun abweichende Einstellungen für Änderungsanträge gesetzt werden können. So ist es beispielsweise möglich, dass es zum Stellen von regulären Anträgen eine Mindestzahl an Unterstützenden geben muss, während Änderungsanträge auch von Einzelpersonen gestellt werden können.</p>
 </div>
--- plugins/antragsgruen_sites/views/manager/help_amendments_en.php
+++ plugins/antragsgruen_sites/views/manager/help_amendments_en.php
@@ -29,7 +29,7 @@ $controller->layoutParams->addBreadcrumb('Amendments');
         </li>
     </ul>
 
-    <p><strong>Introduction</strong></p>
+    <h2 id="introduction">Introduction</h2>
 
     <p>In this tutorial, we first introduce how amending a document looks from the point of view of a regular user. Then we will explain some of the most important ways that administrators can adapt the amendment flow to the requirements of their respective organizations.</p>
 
@@ -59,5 +59,50 @@ $controller->layoutParams->addBreadcrumb('Amendments');
 
     <p>This way, users like delegates can easily read the motion, draft resolution or draft party platform, while seeing which parts might be controversial or where suggestions / amendments exist.</p>
 
-    <p>TO BE CONTINUED...</p>
+    <h2 id="setup">Setting up amendments</h2>
+
+    <p>To let users create amendments, you will of course first need an amendable document. This could be a motion or petition by another users, or a text provided by the board of an organization, like the draft for a resolution.</p>
+
+    <p>Strictly speaking, there is also a third option: amendments to the statutes. Here, there is also an amendable base document (a statutory document of the organization), but that document will not be displayed like a regular motion / resolution on the home page. Instead, amendments to that document will be displayed the same way as stand-alone motions. We will explain statutory amendments more in detail in a later tutorial.</p>
+
+    <p>We <a href="/help/member-motion">already explained</a> how to allow users to submit their own motions. If the documents are provided by admins instead (that is, the permission to submit a motion is set to „Nobody“ or „Admins“), you will find the link to create the base document in the admin motion list, at the top of the page a prominent „Create“ button. There, you will have the choice which type of motion to create, independent of any permissions.</p>
+
+    <p><strong>A warning regarding long texts</strong>: when a rather long text is to be published on Antragsgrün, with the option for members to suggest amendments, we strongly encourage to not publish the whole text in one large page, but to split it into several chapters and have each chapters be its own document. Not only is a 100 page document hard to handle for users, if it’s all on one page. Also, for the server that will host Antragsgrün, the load increases with both the length of the document, as well as with the number of amendments per document. Ten documents with 10 pages and 20 amendments each are therefore way less likely to cause troubles for the server than one document of 100 pages and 200 amendments!</p>
+
+    <p>A hint regarding custom document templates: if you create a custom template with different kinds of section types (like PDF attachments or image uploads), please mind that only sections of the simple type „Text“ and „Title“ are amendable, and only if they are marked as such via the checkbox „In Amendments“. There is no issue with having multiple text sections being amendable, if wanted, though. The default template for motions has the „Reason“ section set to be non-amendable, as this is how most organizations have their procedures set up regards to amendments, but this can be set up differently by simply activating the „In Amendments“ checkbox.</p>
+
+    <h3 id="permissions">Permissions</h3>
+
+    <p>Setting up who is allowed to create amendments for a motion of a particular motion type is very similar to setting up who is allowed to create the motion itself. That is, at the motion type under „Permissions“ -> „Create amendments“. A more detailed explanation of the different options can be found in the tutorial on how to set up motions.</p>
+
+    <h3 id="singleparagraph">Restricting amendments to one paragraph</h3>
+
+    <p>Next to the permissions, there is an additional option for amendments: „Amendments may only change one paragraph“. As the name already indicates, it controls if members can create amendments that are changing the text all over the document, or if they need to restrict themselves to one paragraph that has to be chosen by the user before modifying it. Which version to prefer depends a lot on the dynamics of the respective organization and the kinds of changes that are to be encouraged by the system:</p>
+    <ul>
+        <li>Only one paragraph may be amended (at a time): this option is typically chosen to encourage users to make smaller, local changes. If a user wants to change multiple paragraphs, they need to submit multiple amendments, one per amended paragraph.</li>
+        <li>Multiple paragraphs may be amended: this allows more complex amendments, especially also amendments that affect the structure of the document (like moving an aspect of a motion from one paragraph to another one).</li>
+    </ul>
+
+    <p>If the amendment is restricted to one paragraph, this can be restricted even further by selecting „…and only one specific place“. This can be chosen if only single words or phrases are to be changed. As this is very restrictive, we recommend to not use this unless amendments are really only to change individual words, numbers etc.</p>
+
+    <h3 id="globalalternatives">Global Alternatives / Editorial Hints</h3>
+
+    <p>There are two special kinds of amendments for simplifying the workflows and reduce visual clutter, that need to be explicitly activated by the admin though.</p>
+
+    <p>„<strong>Global Alternative</strong>“ is a flag that users (or admins, afterwards) can set for an amendment to indicate that this amendment so fundamentally changes the motion that there is no point in trying to show it in„Change Mode“. These amendments are typically created by the user by removing the whole motion text and replacing it by a complete new text. In the regular motion view, under normal circumstances, this would appear as a change at every single paragraph (deleting that paragraph), and while merging amendments into the text, it would be mutually exclusive with each other amendment. Therefore, by marking an amendment as global alternative, it will not be appear as a bookmark right of the motion text anymore, and be listed as alternative at the bottom of the motion text instead. It will also not be suggested to be merged into the motion text.<br>
+        Mind that an amendment can only be marked as a global alternative if amendments are not restricted to one paragraph.</p>
+
+    <p><strong>Editorial Hints</strong> are amendments that are referring to rather stylistic or structural issues that would involve many different changes. They are not submitted by the user to actually make all necessary changes, but by asking the editorial team in charge of creating the final resolution based on the draft to do these changes, in natural language. This puts extra work on the editorial staff in theory, but is typically still better than having an amendment that changes dozens of words at multiple different locations, making it harder for other users to read the motion text with all its amendment suggestions, and more error-prone when merging the amendments into the final resolution (due to more potential for conflicts with other amendments).</p>
+
+    <p>Both options can be enabled and disabled in the consultation settings (not at the motion type, for a change). More specifically, at „Settings“ -> „This Consultation“ -> „Amendments“, the two points:</p>
+    <ul>
+        <li>Allow global alternatives</li>
+        <li>Allow editorial hints.</li>
+    </ul>
+
+    <h3 id="supporters">Who may submit amendments / Supporters</h3>
+
+    <p>For amendments, there are the same options to specify the information the submitting person needs to provide as for motions. Also the possibilities to ask for supporters, including an explicit supporting phase, are the same for amendments. So for more information about this, we are referring to the <a href="/help/member-motion">tutorial on how to create motions</a>.</p>
+
+    <p>However, mind that by default, motions and amendments based on them have the very same setting within a motion type. So if you specify to ask for a given amount of supporters to submit a motion, the same procedure will apply for amendments. If you need different rules for amendments, remove the checkbox at „Use same settings for amendments“ when configuring the Proposer / Supporters in the motion type. When removing it, a new section will appear, where you can set up exactly the same, only this time for amendments in particular. This way, one could set up to require a given number of supporters to submit a motion, while allowing single members (without having to look for supporters) to submit amendments.</p>
 </div>
