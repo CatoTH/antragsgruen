917c24c35c180181f631561f44bb36164a1dce62
Tobias Hößl <tobias@hoessl.eu>
2024-11-17 11:43:48 +0100

    Silence PHP 8.4 warning - waiting for #980

--- components/Captcha.php
+++ components/Captcha.php
@@ -23,7 +23,7 @@ class Captcha
         $builder = new CaptchaBuilder();
         $builder->distort = true;
         $builder->bgColor = '#fff';
-        $builder->build(300, 80);
+        @$builder->build(300, 80); // https://github.com/CatoTH/antragsgruen/issues/980
 
         $phrase = $builder->phrase;
         RequestContext::getSession()->set('captcha', $phrase);
