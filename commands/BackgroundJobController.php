d33e4807d7d9c7fd8cfe93c98f7c76f42b73f495
Tobias Hößl <tobias@hoessl.eu>
2024-12-01 12:58:47 +0100

    Data stucture for background jobs

---
+++ commands/BackgroundJobController.php
@@ -0,0 +1,45 @@
+<?php
+
+declare(strict_types=1);
+
+namespace app\commands;
+
+use app\components\BackgroundJobProcessor;
+use yii\console\Controller;
+
+/**
+ * Run background commends
+ */
+class BackgroundJobController extends Controller
+{
+    private const MAX_EVENTS = 1000;
+    private const MAX_RUNTIME_SECONDS = 600;
+    private const MAX_MEMORY_USAGE = 64_000_000;
+
+    /**
+     * Runs the background job processor
+     *
+     * @throws \yii\db\Exception
+     */
+    public function actionRun(): void
+    {
+        $processor = new BackgroundJobProcessor(\Yii::$app->getDb());
+        while (!$this->needsRestart($processor)) {
+            $row = $processor->getJobAndSetStarted();
+            if ($row) {
+                $processor->processRow($row);
+            } else {
+                usleep(100000);
+            }
+
+            file_put_contents('/tmp/memory_usage.log', memory_get_peak_usage() . "\n", FILE_APPEND);
+        }
+    }
+
+    private function needsRestart(BackgroundJobProcessor $processor): bool
+    {
+        return $processor->getProcessedEvents() >= self::MAX_EVENTS
+            || $processor->getRuntimeInSeconds() >= self::MAX_RUNTIME_SECONDS
+            || memory_get_peak_usage() >= self::MAX_MEMORY_USAGE;
+    }
+}
