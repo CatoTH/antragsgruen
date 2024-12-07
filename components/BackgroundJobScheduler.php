78134ed3163d34b9fb0accf6ebfffdecd1fda49a
Tobias Hößl <tobias@hoessl.eu>
2024-12-07 11:33:27 +0100

    Usage within supervisord

--- components/BackgroundJobScheduler.php
+++ components/BackgroundJobScheduler.php
@@ -5,19 +5,24 @@ declare(strict_types=1);
 namespace app\components;
 
 use app\models\backgroundJobs\IBackgroundJob;
+use app\models\settings\AntragsgruenApp;
 
 class BackgroundJobScheduler
 {
     public static function executeOrScheduleJob(IBackgroundJob $job): void
     {
-        \Yii::$app->getDb()->createCommand(
-            'INSERT INTO backgroundJob (`siteId`, `consultationId`, `type`, `dateCreation`, `payload`) VALUES (:siteId, :consultationId, :type, NOW(), :payload)',
-            [
-                ':siteId' => $job->getSite()?->id,
-                ':consultationId' => $job->getConsultation()?->id,
-                ':type' => $job->getTypeId(),
-                ':payload' => $job->toJson(),
-            ]
-        )->execute();
+        if (AntragsgruenApp::getInstance()->backgroundJobs) {
+            \Yii::$app->getDb()->createCommand(
+                'INSERT INTO `backgroundJob` (`siteId`, `consultationId`, `type`, `dateCreation`, `payload`) VALUES (:siteId, :consultationId, :type, NOW(), :payload)',
+                [
+                    ':siteId' => $job->getSite()?->id,
+                    ':consultationId' => $job->getConsultation()?->id,
+                    ':type' => $job->getTypeId(),
+                    ':payload' => $job->toJson(),
+                ]
+            )->execute();
+        } else {
+            $job->execute();
+        }
     }
 }
