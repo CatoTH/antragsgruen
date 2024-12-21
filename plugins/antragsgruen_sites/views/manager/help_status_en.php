8890eec8094cc9a148b209b915081c165c0b2b9f
Tobias Hößl <tobias@hoessl.eu>
2024-12-21 10:55:37 +0100

    Translate status help

--- plugins/antragsgruen_sites/views/manager/help_status_en.php
+++ plugins/antragsgruen_sites/views/manager/help_status_en.php
@@ -35,158 +35,160 @@ $controller->layoutParams->fullWidth = true;
         </colgroup>
         <thead>
         <tr>
-            <th>Name</th>
+            <th>Title</th>
             <th>Visible</th>
             <th>Description</th>
         </tr>
         </thead>
         <tbody>
         <tr>
-            <th>STATUS_DRAFT</th>
-            <td>Nein</td>
-            <td></td>
+            <th>Draft</th>
+            <td>No</td>
+            <td>A user started the process to submit a motion, but has not actually submitted it yet. This might be intentional, or accidentally.</td>
         </tr>
         <tr>
-            <th>STATUS_DRAFT_ADMIN</th>
-            <td>Nein</td>
-            <td></td>
+            <th>Entwurf (Admin)</th>
+            <td>No</td>
+            <td>This status can be set by an admin to have a motion be invisible, yet not show up on the To-Do list.</td>
         </tr>
         <tr>
-            <th>STATUS_SUBMITTED_UNSCREENED</th>
-            <td>Nein <sup>[1]</sup></td>
-            <td></td>
+            <th>Submitted (unpublished)</th>
+            <td>No <sup>[1]</sup></td>
+            <td>This status is relevant if motions/amendments are being reviewed before publication. In this case, this status indicates that a motion has been submitted and is awaiting admin review.</td>
         </tr>
         <tr>
-            <th>STATUS_SUBMITTED_UNSCREENED_CHECKED</th>
-            <td>Nein <sup>[1]</sup></td>
-            <td></td>
+            <th>Submitted (reviewed, not yet published)</th>
+            <td>No <sup>[1]</sup></td>
+            <td>This status can be set by an admin to indicate that a motion/amendment has been reviewed but should not be visible yet (e.g. when all documents are to be published at the same time).</td>
         </tr>
         <tr>
-            <th>STATUS_SUBMITTED_SCREENED</th>
-            <td>Ja</td>
-            <td></td>
+            <th>Published</th>
+            <td>Yes</td>
+            <td>Published and visible. This is the default status once a motion or amendment has been published.</td>
         </tr>
         <tr>
-            <th>STATUS_RESOLUTION_PRELIMINARY</th>
-            <td>Ja</td>
-            <td></td>
+            <th>Resolution (preliminary)</th>
+            <td>Yes</td>
+            <td>A preliminary resolution. Functionally, there is no difference to a (non-preliminary) resolution. The additional word just indicates to readers that there might still be editorial changes before the official resolution.</td>
         </tr>
         <tr>
-            <th>STATUS_RESOLUTION_FINAL</th>
-            <td>Ja</td>
-            <td></td>
+            <th>Resolution</th>
+            <td>Yes</td>
+            <td>The final resolution.</td>
         </tr>
         <tr>
-            <th>STATUS_WITHDRAWN</th>
-            <td>Ja</td>
-            <td></td>
+            <th>Withdrawn</th>
+            <td>Yes</td>
+            <td>This motion / amendment was withdrawn by the proposer, but is still visible.</td>
         </tr>
         <tr>
-            <th>STATUS_WITHDRAWN_INVISIBLE</th>
-            <td>Nein</td>
-            <td></td>
+            <th>Withdrawn (invisible)</th>
+            <td>No</td>
+            <td>This motion / amendment was withdrawn by the proposer and is not visible anymore.</td>
         </tr>
         <tr>
-            <th>STATUS_OBSOLETED_BY_MOTION</th>
-            <td>Ja <sup>[2]</sup></td>
-            <td></td>
+            <th>Handled by another motion</th>
+            <td>Yes <sup>[2]</sup></td>
+            <td>This motion / amendment becomes obsolete by a different motion. The new motion can be selected in a separate dropdown.</td>
         </tr>
         <tr>
-            <th>STATUS_OBSOLETED_BY_AMENDMENT</th>
-            <td>Ja <sup>[2]</sup></td>
-            <td></td>
+            <th>Handled by another amendment</th>
+            <td>Yes <sup>[2]</sup></td>
+            <td>This motion / amendment becomes obsolete by a different amendment. The new amendment can be selected in a separate dropdown</td>
         </tr>
         <tr>
-            <th>STATUS_VOTE</th>
-            <td>Ja</td>
-            <td></td>
+            <th>Vote</th>
+            <td>Yes</td>
+            <td>There will be a voting if this motion / amendment will be adopted or not.</td>
         </tr>
         <tr>
-            <th>STATUS_ACCEPTED</th>
-            <td>Ja</td>
-            <td></td>
+            <th>Accepted</th>
+            <td>Yes</td>
+            <td>There was a voting and the necessary majority has been reached. This is being set automatically when closing a voting.</td>
         </tr>
         <tr>
-            <th>STATUS_REJECTED</th>
-            <td>Ja</td>
-            <td></td>
+            <th>Rejected</th>
+            <td>Yes</td>
+            <td>There was a voting and the necessary majority has NOT been reached. This is being set automatically when closing a voting.</td>
         </tr>
         <tr>
-            <th>STATUS_QUORUM_MISSED</th>
-            <td>Ja</td>
-            <td></td>
+            <th>Quorum missed</th>
+            <td>Yes</td>
+            <td>There was a voting and the necessary quorum has been reached. This is being set automatically when closing a voting.</td>
         </tr>
         <tr>
-            <th>STATUS_QUORUM_REACHED</th>
-            <td>Ja</td>
-            <td></td>
+            <th>Quorum reached</th>
+            <td>Yes</td>
+            <td>There was a voting and the necessary quorum has NOT been reached. This is being set automatically when closing a voting.</td>
         </tr>
         <tr>
-            <th>STATUS_MODIFIED_ACCEPTED</th>
-            <td>Ja</td>
-            <td></td>
+            <th>Accepted (modified)</th>
+            <td>Yes</td>
+            <td>As status, this is purely informational status without special functionality. Relevant mostly as proposed procedure.</td>
         </tr>
         <tr>
-            <th>STATUS_MODIFIED</th>
-            <td>Ja</td>
-            <td></td>
+            <th>Modified</th>
+            <td>Yes</td>
+            <td><em>Purely informational status without special functionality</em></td>
         </tr>
         <tr>
-            <th>STATUS_ADOPTED</th>
-            <td>Ja</td>
-            <td></td>
+            <th>Adopted</th>
+            <td>Yes</td>
+            <td><em>Purely informational status without special functionality</em></td>
         </tr>
         <tr>
-            <th>STATUS_PAUSED</th>
-            <td>Ja</td>
-            <td></td>
+            <th>Paused</th>
+            <td>Yes</td>
+            <td><em>Purely informational status without special functionality</em></td>
         </tr>
         <tr>
-            <th>STATUS_COMPLETED</th>
-            <td>Ja</td>
-            <td></td>
+            <th>Completed</th>
+            <td>Yes</td>
+            <td><em>Purely informational status without special functionality</em></td>
         </tr>
         <tr>
-            <th>STATUS_MISSING_INFORMATION</th>
-            <td>Ja</td>
-            <td></td>
+            <th>Missing information</th>
+            <td>Yes</td>
+            <td><em>Purely informational status without special functionality</em></td>
         </tr>
         <tr>
-            <th>STATUS_DISMISSED</th>
-            <td>Ja</td>
-            <td></td>
+            <th>Dismissed</th>
+            <td>Yes</td>
+            <td><em>Purely informational status without special functionality</em></td>
         </tr>
         <tr>
-            <th>STATUS_PROCESSED</th>
-            <td>Ja</td>
-            <td></td>
+            <th>Processed</th>
+            <td>Yes</td>
+            <td><em>Purely informational status without special functionality/em></td>
         </tr>
         <tr>
-            <th>STATUS_INLINE_REPLY</th>
-            <td>Ja</td>
-            <td></td>
+            <th>Response</th>
+            <td>Yes</td>
+            <td><em>Purely informational status without special functionality</em></td>
         </tr>
         <tr>
-            <th>STATUS_MOVED</th>
-            <td>Ja</td>
-            <td></td>
+            <th>Moved</th>
+            <td>Yes</td>
+            <td>A placeholder motion at its original position, to set a link to a different position after this motion has been moved to a different consultation or agenda item.</td>
         </tr>
         <tr>
-            <th>STATUS_PROPOSED_MOVE_TO_OTHER_MOTION</th>
-            <td>Nein</td>
-            <td></td>
+            <th>Proposed move from other motion</th>
+            <td>No</td>
+            <td>Internal status for amendments, to indicate that an amendment to a different motion is to be moved here.</td>
         </tr>
         <tr>
-            <th>STATUS_CUSTOM_STRING</th>
-            <td>Ja</td>
-            <td></td>
+            <th>Custom status</th>
+            <td>Yes</td>
+            <td>Placeholder status. After selecting this, it is possible to enter an arbitrary status name that is not available in the list above.</td>
         </tr>
         </tbody>
     </table>
 
     <p>
-        <sup>[1]</sup> screeningMotionsShown<br>
-        <sup>[2]</sup> obsoletedByMotionsShown
+        <sup>[1]</sup> Not visible by default. It can be set up to show these motions in an half-transparent font at: This Consultation -> Motions<br>
+    </p>
+    <p>
+        <sup>[2]</sup> Visible by default, but can be changed by hand in the database to invisible.
     </p>
 </div>
 
