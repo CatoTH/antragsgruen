e7539c8cb31502d8a464e060d375eca34352d0f7
Tobias Hößl <tobias@hoessl.eu>
2025-02-01 16:55:20 -0500

    Use nested-draggable-plus

--- views/admin/index/agenda.php
+++ views/admin/index/agenda.php
@@ -18,7 +18,7 @@ $layout->addCSS('css/backend.css');
 $layout->addVueTemplate('@app/views/admin/index/agenda.vue.php');
 $layout->loadSortable();
 $layout->loadVue();
-$layout->loadVueDraggable();
+$layout->loadVueDraggablePlus();
 
 $this->title = 'Tagesordnung';
 $layout->addBreadcrumb(Yii::t('admin', 'bread_settings'), UrlHelper::createUrl('admin/index'));
@@ -29,10 +29,14 @@ $layout->addBreadcrumb('Tagesordnung');
 <div class="content">
 
     <div class="agendaEditForm"
-         data-antragsgruen-widget="backend/AgendEditVue"
+         data-antragsgruen-widget="backend/AgendaEditVue"
          data-agenda="<?= Html::encode(json_encode([
-             ["id" => 1, "title" => "TOP 1"],
-             ["id" => 2, "title" => "TOP 2"],
+             ["id" => 1, "title" => "TOP 1", "children" => []],
+             ["id" => 2, "title" => "TOP 2", "children" => [
+                 ["id" => 3, "title" => "TOP 2.1", "children" => []],
+                 ["id" => 4, "title" => "TOP 2.2", "children" => []],
+             ]],
+             ["id" => 5, "title" => "TOP 3", "children" => []],
          ])) ?>">
         <div class="agendaEdit"></div>
     </div>
