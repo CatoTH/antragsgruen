632e8f6a8ad1be7f12279f00789421727489d62f
Tobias Hößl <tobias@hoessl.eu>
2025-02-16 16:45:42 +0100

    Restrict movement of dates, populate agenda from consultation

--- views/admin/index/agenda.php
+++ views/admin/index/agenda.php
@@ -24,20 +24,15 @@ $this->title = 'Tagesordnung';
 $layout->addBreadcrumb(Yii::t('admin', 'bread_settings'), UrlHelper::createUrl('admin/index'));
 $layout->addBreadcrumb('Tagesordnung');
 
+$apiModel = \app\models\api\AgendaItem::getItemsFromConsultation($consultation);
+
 ?><h1><?= Yii::t('admin', 'con_h1') ?></h1>
 
 <div class="content">
 
     <div class="agendaEditForm"
          data-antragsgruen-widget="backend/AgendaEditVue"
-         data-agenda="<?= Html::encode(json_encode([
-             ["id" => 1, "title" => "TOP 1", "children" => []],
-             ["id" => 2, "title" => "TOP 2", "children" => [
-                 ["id" => 3, "title" => "TOP 2.1", "children" => []],
-                 ["id" => 4, "title" => "TOP 2.2", "children" => []],
-             ]],
-             ["id" => 5, "title" => "TOP 3", "children" => []],
-         ])) ?>">
+         data-agenda="<?= Html::encode(json_encode($apiModel)) ?>">
         <div class="agendaEdit"></div>
     </div>
 </div>
