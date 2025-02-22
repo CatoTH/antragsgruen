55682773f8b72f6fd0f6793f4ac15e761710888a
Tobias Hößl <tobias@hoessl.eu>
2025-02-22 20:15:49 +0100

    Initial datetime integration

---
+++ views/shared/datetime_selector.vue.php
@@ -0,0 +1,66 @@
+<script>
+    const dateTimePickerComponent = {
+        template: '<div class="input-group date datetimepicker">\
+            <input type="text" class="form-control" value="" autocomplete="off">\
+            <span class="input-group-addon" aria-hidden="true"><span class="glyphicon glyphicon-calendar" aria-hidden="true"></span></span>\
+        </div>',
+        props: {
+            modelValue: {
+                type: String,
+                default: null
+            },
+            type: {
+                type: String,
+                default: null
+            },
+            locale: {
+                type: String,
+                default: null
+            }
+        },
+        watch: {
+            value: function (newVal, oldVal) {
+                console.log(newVal);
+            }
+        },
+        data() {
+            return {
+                pickerElement: null
+            };
+        },
+        computed: {},
+        methods: {
+            initElement: function () {
+                var format = '';
+                if (this.type === 'date') {
+                    format = 'dddd, Do MMMM YYYY';
+                }
+                if (this.type === 'time') {
+                    format = 'LT';
+                }
+
+                let initDate = null;
+                if (this.modelValue) {
+                    initDate = moment(this.modelValue, "YYYY-MM-DD", this.locale);
+                }
+
+                this.pickerElement = $(this.$el).datetimepicker({
+                    locale: this.locale,
+                    format: format,
+                    defaultDate: initDate
+                });
+                this.pickerElement.on("dp.change", (newDate) => {
+                    this.$emit('update:modelValue', newDate.date.format("YYYY-MM-DD"));
+                })
+            }
+        },
+        mounted() {
+            this.initElement();
+        },
+        beforeUnmount() {
+            this.pickerElement.datetimepicker("destroy");
+        }
+    };
+
+    __setVueComponent('agenda', 'component', 'v-datetime-picker', dateTimePickerComponent);
+</script>
