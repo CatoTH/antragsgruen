cdff4152468dabee4ffa00ba7bc0610aa4f29f1d
Tobias Hößl <tobias@hoessl.eu>
2025-03-31 20:34:05 +0200

    Finish saving

--- models/forms/AgendaSaver.php
+++ models/forms/AgendaSaver.php
@@ -55,19 +55,13 @@ class AgendaSaver
                 $dbItem->removeSpeakingListsIfPossible();
             }
 
-            /*
-
-            if (isset($data['time']) && preg_match('/^(?<hour>\d\d):(?<minute>\d\d)(?<ampm> (AM|PM))?$/siu', $data['time'], $matches)) {
-                $hour = $matches['hour'];
-                if (isset($matches['ampm']) && trim(strtolower($matches['ampm'])) === 'pm') {
-                    $hour = (string)((int)$hour + 12);
-                }
-                $item->time = $hour . ':' . $matches['minute'];
+            if ($apiItem->date && preg_match('/^\d{4}-\d{2}-\d{2}$/', $apiItem->date)) {
+                $dbItem->time = $apiItem->date;
+            } elseif ($apiItem->time && preg_match('/^\d+:\d{2}$/', $apiItem->time)) {
+                $dbItem->time = $apiItem->time;
             } else {
-                $item->time = null;
+                $dbItem->time = null;
             }
-            Motion Type
-            */
 
             try {
                 if (count($apiItem->settings->motionTypes) > 0) {
