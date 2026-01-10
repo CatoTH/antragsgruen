339862017e4e0ea3b2ea1b9eb1eacf505bf4b098
Tobias Hößl <tobias@hoessl.eu>
2026-01-10 18:25:32 -0500

    Raise minimum PHP level to 8.2

--- config/defines.php
+++ config/defines.php
@@ -3,6 +3,6 @@
 define('ANTRAGSGRUEN_VERSION', '4.16.2');
 define('ANTRAGSGRUEN_HISTORY_URL', 'https://github.com/CatoTH/antragsgruen/blob/main/History.md');
 define('ANTRAGSGRUEN_UPDATE_BASE', 'https://antragsgruen.de/updates/');
-define('ANTRAGSGRUEN_MIN_PHP_VERSION', '8.1.0');
+define('ANTRAGSGRUEN_MIN_PHP_VERSION', '8.2.0');
 define('ANTRAGSGRUEN_NEXT_PHP_VERSION', '8.2.0');
 define('ANTRAGSGRUEN_INSTALLATION_SOURCE', 'repository-source');
