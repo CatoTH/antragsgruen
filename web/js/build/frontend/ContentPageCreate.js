d7cc4a82599ea0e40aa9332412be5e19f0f35544
Tobias Hößl <tobias@hoessl.eu>
2026-02-28 09:19:46 -0500

    Initial attempt to migrate to JS Modules

--- web/js/build/frontend/ContentPageCreate.js
+++ web/js/build/frontend/ContentPageCreate.js
@@ -1,2 +1,2 @@
-define(["require","exports"],function(e,t){"use strict";Object.defineProperty(t,"__esModule",{value:!0}),t.ContentPageCreate=void 0;t.ContentPageCreate=class{constructor(e){this.$form=e,this.$form.find("input[name=url]").on("keyup change keypress",e=>{let t=$(e.currentTarget);t.val(t.val().replace(/[^\w_\-,\.äöüß]/g,""))})}}});
+export class ContentPageCreate{constructor(e){this.$form=e,this.$form.find("input[name=url]").on("keyup change keypress",e=>{let r=$(e.currentTarget);r.val(r.val().replace(/[^\w_\-,\.äöüß]/g,""))})}}
 //# sourceMappingURL=ContentPageCreate.js.map
--- web/js/build/installation/InitSite.js
+++ web/js/build/installation/InitSite.js
@@ -1,2 +1,2 @@
-define(["require","exports","../shared/SiteCreateWizard"],function(e,r){"use strict";Object.defineProperty(r,"__esModule",{value:!0}),new SiteCreateWizard($("form.siteCreate"))});
+import"../shared/SiteCreateWizard";new SiteCreateWizard($("form.siteCreate"));
 //# sourceMappingURL=InitSite.js.map
