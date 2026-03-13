a197f7c1ead0e3ac304c3f157418eb7d75f42b03
Tobias Hößl <tobias@hoessl.eu>
2026-03-13 19:57:55 -0400

    Migrate CopyUrl

---
+++ web/js/modules/frontend/CopyUrlToClipboard.js
@@ -0,0 +1,16 @@
+export function copyUrlToClipboard(widget) {
+    const $widget = $(widget),
+        $button = $widget.find("button"),
+        clipboard = new ClipboardJS($button[0]);
+
+    $widget.find(".clipboard-done").addClass("hidden");
+    clipboard.on('success', () => {
+        $widget.find(".form-group").addClass("has-success has-feedback");
+        $widget.find(".clipboard-done").removeClass("hidden");
+        $button.trigger("focus");
+    });
+
+    clipboard.on('error', () => {
+        alert("Could not copy the URL to the clipboard");
+    });
+}
