35a0db2179e14c825df72a6deaf849cb29f03733
Tobias Hößl <tobias@hoessl.eu>
2026-04-26 20:14:42 -0400

    Fix composer require command - fixes #1131

--- docker/initialize-development-environment.sh
+++ docker/initialize-development-environment.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 composer install
 echo "CREATE DATABASE antragsgruen" | mysql -h mysql -u root -proot
-composer require open-telemetry/sdk open-telemetry/exporter-otlp open-telemetry/opentelemetry-auto-pdo open-telemetry/opentelemetry-auto-curl open-telemetry/opentelemetry-auto-guzzle php-http/guzzle7-adapter -y
+composer require open-telemetry/sdk open-telemetry/exporter-otlp open-telemetry/opentelemetry-auto-pdo open-telemetry/opentelemetry-auto-curl open-telemetry/opentelemetry-auto-guzzle php-http/guzzle7-adapter
 /var/www/antragsgruen/yii database/create-test --interactive=0
