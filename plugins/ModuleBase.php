25db3eb21461f8cddaeea46d276f709007c56b39
Tobias Hößl <tobias@hoessl.eu>
2026-05-02 11:00:26 -0400

    Allow plugins to override default motion version

--- plugins/ModuleBase.php
+++ plugins/ModuleBase.php
@@ -348,6 +348,11 @@ class ModuleBase extends Module
         return null;
     }
 
+    public static function getInitialMotionVersion(Consultation $consultation, Motion $partiallyInitializedMotion): ?string
+    {
+        return null;
+    }
+
     public static function getMotionVersions(Consultation $consultation): ?array
     {
         return null;
