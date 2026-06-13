f46a6c3d767e8b24c45dfd3591cf54fe0c338ff2
Tobias Hößl <tobias@hoessl.eu>
2026-06-13 07:28:46 -0400

    Make code in /config survive updates

---
+++ docker/apache-single-container/entrypoint.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+# config/ is a mounted volume typically, to have config.json changes survive container rebuilds.
+# This means, that updated files in the image will be hidden. Hence, we move those files to config-defaults in the image build
+# and copy them into config/ on container startup.
+#!/bin/sh
+for f in /var/www/html/config-defaults/*; do
+  fname=$(basename "$f")
+    target="/var/www/html/config/$fname"
+
+    # Just copy it when it actually changed - not on every container start
+    if [ ! -f "$target" ] || [ "$(md5sum < "$f")" != "$(md5sum < "$target")" ]; then
+      cp "$f" "$target"
+    fi
+done
+
+exec "$@"
