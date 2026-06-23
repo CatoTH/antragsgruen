93605bd4d701f838e2d3cfd93fa6960d5aa70cc2
Tobias Hößl <tobias@hoessl.eu>
2026-06-22 21:20:37 -0400

    Support public/private keys

--- components/JwtCreator.php
+++ components/JwtCreator.php
@@ -9,6 +9,16 @@ use app\models\db\{Consultation, User};
 use Firebase\JWT\{ExpiredException, Key, SignatureInvalidException, JWT};
 use app\models\settings\{Privileges, AntragsgruenApp};
 
+/**
+ * JWTs have two consumers:
+ * - The REST API (always)
+ * - The Live Proxy (optional)
+ *
+ * The Live Proxy relies on RS256 algorithm for JWT, as it only has the Public Key.
+ * RS256 is the recommended way, and is enabled once a private and public key is registered in the configuration.
+ * To also enable the REST API in simple setups, there is a fallback to HS256,
+ * with a combination of the global app secret and the per-user secret being the signing key.
+ */
 class JwtCreator
 {
     private const ROLE_SPEECH_ADMIN = 'ROLE_SPEECH_ADMIN';
@@ -22,9 +32,9 @@ class JwtCreator
     public static function createJwt(Consultation $consultation, string $userId, array $roles = []): string
     {
         $params = AntragsgruenApp::getInstance();
-        if ($params->jwtPrivateKey !== null) {
+        if ($params->jwtPrivateKey !== null && $params->jwtPublicKey !== null) {
             if (!file_exists($params->jwtPrivateKey)) {
-                throw new ConfigurationError('JWT Public key file not found');
+                throw new ConfigurationError('JWT Private key file not found');
             }
             $privateKey = (string)file_get_contents($params->jwtPrivateKey);
             $algorithm = 'RS256';
@@ -75,11 +85,11 @@ class JwtCreator
         }
 
         $params = AntragsgruenApp::getInstance();
-        if ($params->jwtPrivateKey !== null) {
-            if (!file_exists($params->jwtPrivateKey)) {
+        if ($params->jwtPrivateKey !== null && $params->jwtPublicKey !== null) {
+            if (!file_exists($params->jwtPublicKey)) {
                 throw new ConfigurationError('JWT Public key file not found');
             }
-            $privateKey = (string)file_get_contents($params->jwtPrivateKey);
+            $privateKey = (string)file_get_contents($params->jwtPublicKey);
             $key = new Key($privateKey, 'RS256');
         } else {
             $key = new Key($user->getJwtSigningKey(), 'HS256');
@@ -92,7 +102,7 @@ class JwtCreator
         } catch (SignatureInvalidException $e) {
             throw new ApiResponseException('Signature Invalid', 403, $e);
         } catch (\Exception $e) {
-            throw new ApiResponseException('Invalid Token', 403, $e);
+            throw new ApiResponseException('Invalid Token: ' . $e->getMessage(), 403, $e);
         }
 
         if ($decoded->sub !== self::USER_PREFIX_REGULAR . $user->id) {
