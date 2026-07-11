256d9000f1a0720e4a3d1ddede3212715fa52b8b
Tobias Hößl <tobias@hoessl.eu>
2026-07-11 08:04:55 -0400

    First draft of admin widget

---
+++ views/consultation/_index_debate_admin.php
@@ -0,0 +1,58 @@
+<?php
+
+use app\components\{Tools, UrlHelper};
+use app\models\api\debate\DebateState;
+use yii\helpers\Html;
+
+/**
+ * @var \yii\web\View $this
+ * @var \app\models\db\Consultation $consultation
+ */
+
+/** @var \app\controllers\Base $controller */
+$controller = $this->context;
+$layout = $controller->layoutParams;
+
+$layout->addJsTranslation('debate');
+$layout->provideJwt = true;
+
+$initState = Tools::getSerializer()->serialize(DebateState::fromConsultation($consultation), 'json');
+$selectableUrl = UrlHelper::createUrl(['/rest/debate/selectable']);
+
+?>
+<section class="currentDebateAdmin currentSpeechPageWidth" aria-labelledby="currentDebateAdminTitle"
+         data-init-state="<?= Html::encode($initState) ?>"
+         data-selectable-url="<?= Html::encode($selectableUrl) ?>"
+>
+    <h2 class="green" id="currentDebateAdminTitle"><?= Yii::t('debate', 'admin_title') ?></h2>
+    <div class="currentDebateAdminWidget"></div>
+</section>
+
+<script type="module" crossorigin="anonymous">
+    import { createApp, h, resolveComponent } from '/npm/vue.runtime.esm-browser.prod.js';
+    import translateDirective from "/js/vue/Translate.vue.js";
+    import debateAdminWidget from "/js/vue/debate/DebateAdminWidget.js";
+
+    const $element = $('.currentDebateAdmin');
+
+    /** @type {import('vue').App} */
+    const widget = createApp({
+        render() {
+            return h(resolveComponent('debate-admin-widget'), {
+                initState: this.initState,
+                selectableUrl: this.selectableUrl,
+            });
+        },
+        data() {
+            return {
+                initState: $element.data('init-state'),
+                selectableUrl: $element.data('selectable-url'),
+            };
+        }
+    });
+
+    widget.component('debate-admin-widget', debateAdminWidget);
+    widget.directive('t', translateDirective);
+
+    widget.mount('.currentDebateAdmin .currentDebateAdminWidget');
+</script>
---
+++ web_src/js/vue/debate/DebateAdminWidget.vue
@@ -0,0 +1,117 @@
+<template>
+    <div class="currentDebateAdminContent">
+        <nav class="debateAdminTabs">
+            <button v-for="tab in tabs" :key="tab" type="button" class="tab"
+                    :class="{ active: activeTab === tab }" @click="activeTab = tab"
+                    v-t="['debate', 'admin_tab_' + tab]"></button>
+        </nav>
+
+        <section v-if="activeTab === 'debated'" class="content debatedTab">
+            <div v-if="!current" class="nothingDebated" v-t="['debate', 'nothing_debated']"></div>
+            <div v-if="current" class="debatedItem">
+                <div class="title">{{ current.title }}</div>
+                <div class="proposer">
+                    <span v-if="current.initiators_html" class="initiators">
+                        <template v-t="['debate', 'submitted_by', false, {}, ': ']"></template>
+                        <span v-html="current.initiators_html"></span>
+                    </span>
+                    <a v-if="current.url_html" :href="current.url_html" class="fulltextLink">
+                        <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+                        <template v-t="['debate', 'fulltext']"></template>
+                    </a>
+                </div>
+            </div>
+
+            <div v-if="loadError" class="alert alert-danger">{{ loadError }}</div>
+            <template v-if="selectables">
+                <div class="startDebateTitle" v-t="['debate', 'admin_start_debate', false, {}, ':']"></div>
+                <div v-for="group in selectableGroups" :key="group.id" class="selectRow">
+                    <label class="rowLabel" :for="'debateAdminSelect-' + group.id"
+                           v-t="['debate', 'admin_select_' + group.id, false, {}, ':']"></label>
+                    <div class="rowSelect">
+                        <select :id="'debateAdminSelect-' + group.id" class="stdDropdown" v-model="selected[group.id]">
+                            <option v-for="item in group.items" :key="item.target_id" :value="item.target_id">
+                                {{ item.title_with_prefix || item.title }}
+                            </option>
+                        </select>
+                    </div>
+                    <div class="rowButton">
+                        <!-- Placeholder: will call the debate moderation endpoint to start the debate once it exists -->
+                        <button type="button" class="btn btn-default" disabled
+                                v-t="['debate', 'admin_select_do']"></button>
+                    </div>
+                </div>
+            </template>
+        </section>
+
+        <section v-if="activeTab !== 'debated'" class="content underConstruction"
+                 v-t="['debate', 'admin_under_construction']"></section>
+    </div>
+</template>
+
+<script>
+import ApiClient from "/js/vue/ApiClient.vue.js";
+import Translate from "/js/vue/Translate.vue.js";
+
+export default {
+    name: 'DebateAdminWidget',
+    props: {
+        initState: {
+            type: Object,
+            required: true,
+        },
+        selectableUrl: {
+            type: String,
+            required: true,
+        },
+    },
+    data() {
+        return {
+            activeTab: 'debated',
+            tabs: ['debated', 'speech', 'voting', 'protocol'],
+            state: this.initState,
+            selectables: null,
+            selected: {
+                motion: null,
+                amendment: null,
+                agenda_item: null,
+            },
+            loadError: null,
+        };
+    },
+    computed: {
+        current() {
+            return this.state ? this.state.current : null;
+        },
+        selectableGroups() {
+            return [
+                { id: 'motion', items: this.selectables.motions },
+                { id: 'amendment', items: this.selectables.amendments },
+                { id: 'agenda_item', items: this.selectables.agenda_items },
+            ].filter(group => group.items.length > 0);
+        },
+    },
+    methods: {
+        loadSelectables() {
+            ApiClient.authorizedFetch(this.selectableUrl)
+                .then(response => {
+                    if (!response.ok) {
+                        throw new Error('HTTP status ' + response.status);
+                    }
+                    return response.json();
+                })
+                .then(selectables => {
+                    this.selectables = selectables;
+                    this.loadError = null;
+                })
+                .catch(err => {
+                    console.error('Could not load the selectable debate items from the backend', err);
+                    this.loadError = Translate.getTranslation('debate', 'admin_selectables_err');
+                });
+        },
+    },
+    mounted() {
+        this.loadSelectables();
+    },
+};
+</script>
