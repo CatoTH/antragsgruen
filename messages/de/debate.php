754e225adea3eda9ad41a29af86a01beddfa4ed8
Tobias Hößl <tobias@hoessl.eu>
2026-07-11 08:04:55 -0400

    First draft of admin widget

--- messages/de/debate.php
+++ messages/de/debate.php
@@ -26,4 +26,50 @@ return [
         'text' => 'Verfahrensantrag stellen',
         'js' => true,
     ],
+
+    'admin_title'             => 'Aktuell debattiert [Administration]',
+    'admin_tab_debated'       => [
+        'text' => 'Debattierter Antrag',
+        'js' => true,
+    ],
+    'admin_tab_speech'        => [
+        'text' => 'Redeliste',
+        'js' => true,
+    ],
+    'admin_tab_voting'        => [
+        'text' => 'Laufende Abstimmung',
+        'js' => true,
+    ],
+    'admin_tab_protocol'      => [
+        'text' => 'Protokoll',
+        'js' => true,
+    ],
+    'admin_under_construction' => [
+        'text' => 'In Arbeit',
+        'js' => true,
+    ],
+    'admin_start_debate'      => [
+        'text' => 'Debatte beginnen über',
+        'js' => true,
+    ],
+    'admin_select_motion'     => [
+        'text' => 'Antrag',
+        'js' => true,
+    ],
+    'admin_select_amendment'  => [
+        'text' => 'Änderungsantrag',
+        'js' => true,
+    ],
+    'admin_select_agenda_item' => [
+        'text' => 'Tagesordnungspunkt',
+        'js' => true,
+    ],
+    'admin_select_do'         => [
+        'text' => 'Auswählen',
+        'js' => true,
+    ],
+    'admin_selectables_err'   => [
+        'text' => 'Die auswählbaren Einträge konnten nicht geladen werden.',
+        'js' => true,
+    ],
 ];
--- messages/en/debate.php
+++ messages/en/debate.php
@@ -26,4 +26,50 @@ return [
         'text' => 'Raise Secondary Motion',
         'js' => true,
     ],
+
+    'admin_title'             => 'Currently debated [Administration]',
+    'admin_tab_debated'       => [
+        'text' => 'Debated Motion',
+        'js' => true,
+    ],
+    'admin_tab_speech'        => [
+        'text' => 'Speaking List',
+        'js' => true,
+    ],
+    'admin_tab_voting'        => [
+        'text' => 'Ongoing Voting',
+        'js' => true,
+    ],
+    'admin_tab_protocol'      => [
+        'text' => 'Protocol',
+        'js' => true,
+    ],
+    'admin_under_construction' => [
+        'text' => 'Under construction',
+        'js' => true,
+    ],
+    'admin_start_debate'      => [
+        'text' => 'Start Debate over',
+        'js' => true,
+    ],
+    'admin_select_motion'     => [
+        'text' => 'Motion',
+        'js' => true,
+    ],
+    'admin_select_amendment'  => [
+        'text' => 'Amendment',
+        'js' => true,
+    ],
+    'admin_select_agenda_item' => [
+        'text' => 'Agenda Item',
+        'js' => true,
+    ],
+    'admin_select_do'         => [
+        'text' => 'Select',
+        'js' => true,
+    ],
+    'admin_selectables_err'   => [
+        'text' => 'Could not load the list of selectable items.',
+        'js' => true,
+    ],
 ];
