c08d4fb9014a7f3ece7a692734b4dcadc4023eb6
Tobias Hößl <tobias@hoessl.eu>
2026-07-11 14:51:57 -0400

    Allow to override prev/next motions from plugins

--- views/motion/LayoutHelper.php
+++ views/motion/LayoutHelper.php
@@ -1117,6 +1117,13 @@ class LayoutHelper
      */
     public static function getPrevNextLinks(Motion $motion): array
     {
+        foreach (AntragsgruenApp::getActivePlugins() as $plugin) {
+            $override = $plugin::getPrevNextLinks($motion);
+            if ($override !== null) {
+                return $override;
+            }
+        }
+
         $consultation = $motion->getMyConsultation();
 
         $prevMotion = null;
