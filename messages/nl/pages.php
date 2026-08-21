a04fa1e1a255a18b82535b3552e89ac625b1883f
niStee <52573120+niStee@users.noreply.github.com>
2026-08-21 14:21:02 +0200

    i18n(nl): complete missing Dutch translation keys for core and admin modules

--- messages/nl/pages.php
+++ messages/nl/pages.php
@@ -1,73 +1,78 @@
 <?php
 
 return [
-    'list_title'      => 'Pagina\'s',
-    'list_add_std'    => 'Maak een standaardpagina',
-    'list_add_custom' => 'Maak een nieuwe pagina',
-    'list_edit'       => 'Pas pagina\'s aan',
+    'list_title'      => 'Inhoudspagina\'s',
+    'list_add_std'    => 'Standaardpagina aanmaken',
+    'list_add_custom' => 'Nieuwe pagina aanmaken',
+    'list_edit'       => 'Pagina\'s bewerken',
     'list_all_cons'   => 'Alle raadplegingen',
     'list_this_con'   => 'Alleen deze raadpleging',
 
     'content_privacy_title' => 'Privacyverklaring',
     'content_privacy_bread' => 'Privacy',
     'content_welcome'       => 'Welkom',
-    'content_welcome_text'  => '<p>Welkom bij Motion Tools</p>',
-    'content_imprint_title' => 'Contactgegevens',
-    'content_imprint_bread' => 'Contactgegevens',
+    'content_welcome_text'  => '<p>Welkom bij Antragsgrün</p>',
+    'content_imprint_title' => 'Colofon',
+    'content_imprint_bread' => 'Colofon',
     'content_help_title'    => 'Help',
     'content_help_bread'    => 'Help',
     'content_help_place'    => '<p>Help...</p>',
     'content_maint_bread'   => 'Onderhoud',
     'content_maint_title'   => 'Onderhoudsmodus',
-    'content_maint_text'    => '<p>Deze raadpleging is (nog) niet publiek gemaakt.</p>',
+    'content_maint_text'    => '<p>Deze raadpleging is (nog) niet openbaar gemaakt.</p>',
     'content_login_pre'     => 'Inloggen (boven)',
     'content_login_post'    => 'Inloggen (beneden)',
     'content_feeds_title'   => 'RSS-Feeds',
-    'content_feeds_text'    => '<p>RSS-feeds zijn een handige manier om op de hoogte te blijven van veel verschillende nieuwsbronnen..</p>
-        <p>Je gebruikt een RSS-reader programma om te abonneren op allerlei nieuwssites, blogs, webcomics, of Motion Tools-sites die je interessant vindt.</p>
-        <p>Dan laat het programma je een overzicht zien van alle nieuwe artiekelen op deze sites.</p>
-        <p>Hier zijn een aantal programmas en webdiensten om RSS feeds te lezen:</p>
+    'content_feeds_text'    => '<p>RSS-feeds zijn een handige manier om op de hoogte te blijven van veel verschillende nieuwsbronnen.</p>
+        <p>Je gebruikt een RSS-reader om je te abonneren op allerlei nieuwssites, blogs of Antragsgrün-sites die je interessant vindt.</p>
+        <p>Vanaf dat moment toont dit programma een overzicht van alle nieuwe artikelen op deze sites.</p>
+        <p>Een kleine selectie van programma\'s en webdiensten om RSS-feeds te lezen:</p>
         <ul>
-            <li><a href="http://www.rssowl.org/">RSSOwl</a> (Linux, macOS, Windows)</li>
-            <li><a href="https://ranchero.com/netnewswire/">NetNewsWire</a> (macOS)</li>
+            <li><a href="https://support.mozilla.org/nl/kb/hoe-abonneren-op-nieuwsfeeds-en-blogs">Mozilla Thunderbird</a> (Linux, macOS, Windows)</li>
+            <li><a href="https://netnewswire.com/">NetNewsWire</a> (macOS, iOS)</li>
             <li><a href="https://reederapp.com/">Reeder</a> (macOS, iOS)</li>
             <li><a href="https://www.vienna-rss.com/">Vienna</a> (macOS)</li>
-            <li><a href="https://www.microsoft.com/en-us/p/newsflow/9nblggh58s5r">Newsflow</a> (Windows)</li>
-            <li><a href="https://feedly.com/">Feedly</a> (Web-Dienst)</li>
-            <li><a href="https://feedbin.com/">Feedbin</a> (Web-Dienst)</li>
+            <li><a href="https://www.microsoft.com/nl-nl/p/newsflow/9nblggh58s5r">Newsflow</a> (Windows)</li>
+            <li><a href="https://feedly.com/">Feedly</a> (Webdienst)</li>
+            <li><a href="https://feedbin.com/">Feedbin</a> (Webdienst)</li>
         </ul>',
 
-    'images_browse_title' => 'Bekijk de afbeeldingen die voor deze raadpleging zijn geüpload',
+    'images_browse_title' => 'Afbeeldingen bekijken die voor deze raadpleging zijn geüpload',
     'images_none'         => 'Er zijn nog geen afbeeldingen geüpload.',
     'images_delete'       => 'Verwijderen',
-    'images_deleted'      => 'Deze afbeelding is verwijderd.',
+    'images_deleted'      => 'De afbeelding is verwijderd.',
 
-    'settings_allcons' => 'Websitebreed',
-    'settings_inmenu'  => 'In menu laten zien',
+    'settings_allcons' => 'Site-breed',
+    'settings_inmenu'  => 'In menu tonen',
     'settings_url'     => 'URL',
     'settings_title'   => 'Titel',
-    'settings_delete'  => 'Verwijder pagina',
-    'create_btn'       => 'Toevoegen',
-    'menu_add_btn'     => 'Voeg een nieuwe pagina toe aan het menu',
-    'fullscr_select'   => 'Content pagina\'s',
-    'fullscr_split'    => 'Split screen',
+    'settings_delete'  => 'Pagina verwijderen',
+    'create_btn'       => 'Aanmaken',
+    'menu_add_btn'     => 'Nieuwe pagina toevoegen aan het menu',
+    'fullscr_select'   => 'Inhoudspagina\'s',
+    'fullscr_split'    => 'Gesplitst scherm',
 
     'documents_title' => 'Documenten',
-    'documents_intro_text' => 'Op deze pagina worden raadplegingsdocumenten getoond',
-    'documents_add_group_btn' => 'Nieuw bestandsgroep',
-    'documents_group_name' => 'Groepnaam',
-    'documents_group_add' => 'Creëer',
-    'documents_group_delete' => 'Verwijder bestandsgroep',
-    'documents_group_delete_c' => 'Weet je zeker dat je de bestandsgroep en alle inhoud wilt verwijderen?',
-    'documents_file_delete' => 'Verwijder bestand',
-    'documents_file_delete_c' => 'Weet je zeker dat je dit wilt verwijderen?',
-    'documents_group_added' => 'De bestandsgroep is aangemaakt',
+    'documents_intro_text' => 'Op deze pagina worden documenten voor deze raadpleging gepubliceerd.',
+    'documents_add_group_btn' => 'Nieuwe bestandsgroep',
+    'documents_group_name' => 'Groepsnaam',
+    'documents_group_add' => 'Aanmaken',
+    'documents_group_delete' => 'Bestandsgroep verwijderen',
+    'documents_group_delete_c' => 'Weet je zeker dat je de bestandsgroep en alle bijbehorende bestanden wilt verwijderen?',
+    'documents_file_delete' => 'Bestand verwijderen',
+    'documents_file_delete_c' => 'Weet je zeker dat je dit bestand wilt verwijderen?',
+    'documents_group_added' => 'De bestandsgroep is toegevoegd',
     'documents_group_deleted' => 'De bestandsgroep is verwijderd',
     'documents_file_deleted' => 'Het bestand is verwijderd',
     'documents_add_file_btn' => 'Nieuw bestand uploaden',
-    'documents_file_add' => 'Uploaden',
+    'documents_file_add' => 'Bestand uploaden',
     'documents_uploaded_file' => 'Het bestand is geüpload.',
-    'documents_download_all' => 'Download alles',
-    'documents_none_yet' => 'Geen gepubliceerde documenten.',
-    'documents_sort' => 'Sorteer bestandsgroepen',
+    'documents_download_all' => 'Alles downloaden',
+    'documents_none_yet' => 'Er zijn nog geen documenten gepubliceerd.',
+    'documents_sort' => 'Bestandsgroepen sorteren',
+
+    'err_permission' => 'Geen rechten om deze pagina te bewerken',
+    'err_exists_site' => 'Er bestaat al een site-brede inhoudspagina met deze URL.',
+    'err_exists_con' => 'Er bestaat al een inhoudspagina met deze URL voor deze raadpleging.',
+    'err_invalid_caracter' => 'Ongeldig teken in de ingevoerde URL',
 ];
